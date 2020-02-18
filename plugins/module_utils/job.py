3e2bba0e7033c35d9df892d8728a7c4ec039cd93
Blake Becker <blakejordanbecker@gmail.com>
2020-02-18 11:51:04 -0800

    updated parsing of abend codes

--- plugins/module_utils/job.py
+++ plugins/module_utils/job.py
@@ -237,7 +237,7 @@ def _get_return_code_str(rc_str):
         Union[str, NoneType] -- Returns string RC or ABEND code if possible, if not returns NoneType
     """
     rc = None
-    match = re.search(r'(?:\s*CC\s*([0-9]+))|(?:ABEND(\s*(?:S|U)[0-9]+))', rc_str)
+    match = re.search(r'(?:\s*CC\s*([0-9]+))|(?:ABEND\s*((?:S|U)[0-9]+))', rc_str)
     if match:
-        rc = match.group(1)
+        rc = match.group(1) or match.group(2)
     return rc

53534bd12d9e0d2252cc4f3e1994f4231536920a
ddimatos <dimatos@gmail.com>
2020-02-17 17:55:42 -0800

    Updated copyright and license to funtional test cases

--- tests/functional/modules/test_zos_job_query_func.py
+++ tests/functional/modules/test_zos_job_query_func.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+# Copyright (c) IBM Corporation 2019, 2020
+# Apache License, Version 2.0 (see https://opensource.org/licenses/Apache-2.0)
+
 from __future__ import absolute_import, division
 
 import os

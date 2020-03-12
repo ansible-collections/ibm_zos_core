ccbd0f9164c016fb0d6610842b3d23b4fb62bac1
Asif Mahmud <asif.mahmud@ibm.com>
2020-03-11 19:07:51 -0700

    add __future__ imports

--- plugins/action/zos_fetch.py
+++ plugins/action/zos_fetch.py
@@ -1,3 +1,10 @@
+# Copyright (c) IBM Corporation 2019, 2020
+# Apache License, Version 2.0 (see https://opensource.org/licenses/Apache-2.0)
+
+from __future__ import (absolute_import, division, print_function)
+
+__metaclass__ = type
+
 import os
 import subprocess
 import base64

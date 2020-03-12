4bc49c1427e723493f219dff68284ba595a67f64
ddimatos <dimatos@gmail.com>
2020-03-12 16:18:23 -0700

    ansible doc changes to modules for better formatting

--- plugins/modules/zos_job_query.py
+++ plugins/modules/zos_job_query.py
@@ -14,158 +14,126 @@ ANSIBLE_METADATA = {
     "supported_by": "community",
 }
 
-DOCUMENTATION = """
+DOCUMENTATION =r'''
 ---
 module: zos_job_query
-short_description: The module allows you to list z/OS job(s) and the current status of the job(s).
+short_description: Query job(s) and status.
 description:
   - List z/OS job(s) and the current status of the job(s).
   - Uses owner to filter the jobs by the job owner.
   - Uses system to filter the jobs by system where the job is running (or ran) on.
   - Uses job_id to filter the jobs by the job id.
-
 author: "Ping Xiao (@xiaopingBJ)"
 options:
   job_name:
-    description: The job name to query
+    description: 
+       - The job name to query.
     type: str
     required: False
     default: "*"
   owner:
     description:
       - Identifies the owner of the job.
-      - Defaults to the current user
     type: str
     required: False
+    deafault: Current user
   job_id:
     description:
-      - The job number that has been assigned to the job. These normally begin with STC, JOB, TSU and are followed by 5 digits.
+      - The job number that has been assigned to the job. These normally begin 
+        with STC, JOB, TSU and are followed by 5 digits.
     type: str
     required: False
-seealso: []
-notes:
-  - check_mode is supported but in the case of this module, it never changes the system state so always return False
-"""
+'''
 
-EXAMPLES = """
+EXAMPLES =r'''
 - name: list zos jobs with a jobname 'IYK3ZNA1'
   zos_job_query:
     job_name: "IYK3ZNA1"
 
-- Sample result('jobs' field):
-    [
-      {
-        "job_name": "IYK3ZNA1",
-        "owner": "BROWNAD",
-        "job_id": "JOB01427",
-        "ret_code": "null",
-      },
-    ]
-
 - name: list the jobs matching jobname 'IYK3*'
   zos_job_query:
     job_name: "IYK3*"
-
-- Sample result('jobs' field):
-    [
-      {
-        "job_name": "IYK3ZNA1",
-        "owner": "BROWNAD",
-        "job_id": "JOB01427",
-        "ret_code": "null",
-      },
-      {
-        "job_name": "IYK3ZNA2",
-        "owner": "BROWNAD",
-        "job_id": "JOB16577",
-        "ret_code": { "msg": "CANCELED", "code": "null" },
-      },
-    ]
 
 - name: list the job with a jobname 'IYK3ZNA*' and jobid as JOB01427
   zos_job_query:
     job_name: IYK3ZNA*
     job_id: JOB01427
 
-- Sample result('jobs' field):
-    [
-      {
-        "job_name": "IYK3ZNA1",
-        "owner": "BROWNAD",
-        "job_id": "JOB01427",
-        "ret_code": "null",
-      },
-    ]
-
 - name: list the job with a jobname 'IYK3ZNA*' and owner as BROWNAD
   zos_job_query:
     job_name: IYK3ZNA*
     owner: BROWNAD
+'''
 
-- Sample result('jobs' field):
-    [
-      {
-        "job_name": "IYK3ZNA1",
-        "owner": "BROWNAD",
-        "job_id": "JOB01427",
-        "ret_code": "null",
-      },
-      {
-        "job_name": "IYK3ZNA2",
-        "owner": "BROWNAD",
-        "job_id": "JOB16577",
-        "ret_code": { "msg": "CANCELED", "code": "null" },
-      },
-    ]
-"""
-
-RETURN = """
+RETURN =r'''
 changed:
-  description: True if the state was changed, otherwise False
-  returned: always
-  type: bool
-failed:
-  description: True if zos_job_query failed, otherwise False
+  description: 
+     True if the state was changed, otherwise False.
   returned: always
   type: bool
 jobs:
-  description: The list z/OS job(s) and the current status of the job(s)
+  description: 
+     The list of z/OS job(s) and status.
   returned: success
   type: list
   elements: dict
   contains:
     job_name:
-      description: The name of the job
+      description:
+         The name of the batch job.
       type: str
+      sample: IYK3ZNA2
     owner:
-      description: The owner of the job
+      description:
+         The owner who ran the job.
       type: str
+      sample: BROWNAD
     job_id:
-      description: The ID of the job
+      description:
+         Unique job id assigned to the job by JES
       type: str
+      sample: JOB01427
     ret_code:
-      description: The return code information for the job
+      description:
+         Return code output collected from job log.
       type: dict
       contains:
         msg:
-          description: Holds the return code (eg. "CC 0000")
+          description: 
+            Return code or abend resulting from the job submission.
           type: str
+          sample: CC 0000
         code:
-          description: Holds the return code string (eg. "00", "S0C4")
-          type: str
+          description: 
+             Return code converted to integer value (when possible)
+          type: int
+          sample: 00
+      sample:
+         - "code": 0
+         -  "msg": "CC 0000"
+  sample:
+    [
+        {
+            "job_name": "IYK3ZNA1",
+            "owner": "BROWNAD",
+            "job_id": "JOB01427",
+            "ret_code": "null",
+        },
+        {
+            "job_name": "IYK3ZNA2",
+            "owner": "BROWNAD",
+            "job_id": "JOB16577",
+            "ret_code": { "msg": "CANCELED", "code": "null" },
+        },
+    ]
 message:
-  description: Message returned on failure
+  description: 
+     Message returned on failure.
   type: str
   returned: failure
   sample:
-    - changed: false,
-    - failed: false,
-    - msg: "List FAILED! no such job been found: IYK3Z0R9"
-original_message:
-  description: The original input parameters
-  type: str
-  returned: always
-"""
+     msg: "List FAILED! no such job been found: IYK3Z0R9"
+'''
 
 try:
     from zoautil_py import Jobs

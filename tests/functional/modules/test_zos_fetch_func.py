50a36409751962056d31c991dc78843f2abbe5a8
Asif Mahmud <asif.mahmud@ibm.com>
2020-03-23 20:24:00 -0700

    Add test case for empty data sets

--- tests/functional/modules/test_zos_fetch_func.py
+++ tests/functional/modules/test_zos_fetch_func.py
@@ -85,8 +85,7 @@ def test_fetch_ascii_encoded_uss_file(ansible_zos_module):
     params = dict(
         src='/etc/profile',
         dest='/tmp/',
-        flat=True,
-        encoding='ASCII'
+        flat=True
     )
     results = hosts.all.zos_fetch(**params)
     dest_path = '/tmp/profile'
@@ -106,8 +105,7 @@ def test_fetch_sequential_data_set_fixed_block(ansible_zos_module):
     params = dict(
         src='IMSTESTL.IMS01.DDCHKPT',
         dest='/tmp/',
-        flat=True,
-        use_qualifier=False
+        flat=True    
     )
     results = hosts.all.zos_fetch(**params)
     dest_path = '/tmp/IMSTESTL.IMS01.DDCHKPT'
@@ -128,8 +126,7 @@ def test_fetch_sequential_data_set_variable_block(ansible_zos_module):
     params = dict(
         src='IMSTESTL.IMS01.SPOOL1',
         dest='/tmp/',
-        flat=True,
-        use_qualifier=False
+        flat=True
     )
     results = hosts.all.zos_fetch(**params)
     dest_path = '/tmp/IMSTESTL.IMS01.SPOOL1'
@@ -150,8 +147,7 @@ def test_fetch_partitioned_data_set(ansible_zos_module):
     params = dict(
         src='IMSTESTL.COMN91',
         dest='/tmp/',
-        flat=True,
-        use_qualifier=False
+        flat=True
     )
     results = hosts.all.zos_fetch(**params)
     dest_path = '/tmp/IMSTESTL.COMN91'
@@ -173,8 +169,7 @@ def test_fetch_vsam_data_set(ansible_zos_module):
     params = dict(
         src='IMSTESTL.LDS01.WADS2',
         dest='/tmp/',
-        flat=True,
-        use_qualifier=False
+        flat=True
     )
     results = hosts.all.zos_fetch(**params)
     dest_path = '/tmp/IMSTESTL.LDS01.WADS2'
@@ -190,13 +185,12 @@ def test_fetch_vsam_data_set(ansible_zos_module):
             os.remove(dest_path)
 
 
-def test_fetch_partitioned_data_set_member(ansible_zos_module):
+def test_fetch_partitioned_data_set_member_in_binary_mode(ansible_zos_module):
     hosts = ansible_zos_module
     params = dict(
         src='IMSTESTL.COMNUC(ATRQUERY)',
         dest='/tmp/',
         flat=True,
-        use_qualifier=False,
         is_binary=True
     )
     results = hosts.all.zos_fetch(**params)
@@ -220,8 +214,7 @@ def test_fetch_sequential_data_set_in_binary_mode(ansible_zos_module):
         src='IMSTESTL.IMS01.DDCHKPT',
         dest='/tmp/',
         flat=True,
-        is_binary=True,
-        use_qualifier=False
+        is_binary=True
     )
     results = hosts.all.zos_fetch(**params)
     dest_path = '/tmp/IMSTESTL.IMS01.DDCHKPT'
@@ -258,3 +251,102 @@ def test_fetch_partitioned_data_set_binary_mode(ansible_zos_module):
     finally:
         if os.path.exists(dest_path):
             shutil.rmtree(dest_path)
+
+
+def test_fetch_sequential_data_set_empty(ansible_zos_module):
+    hosts = ansible_zos_module
+    params = dict(
+        src='IMSTESTL.IMSCOM.HOLDER',
+        dest='/tmp/',
+        flat=True    
+    )
+    results = hosts.all.zos_fetch(**params)
+    dest_path = '/tmp/IMSTESTL.IMSCOM.HOLDER'
+    try:
+        for result in results.contacted.values():
+            assert result.get('changed') is True
+            assert result.get('data_set_type') == 'Sequential'
+            assert result.get('module_stderr') is None
+            assert result.get('dest') == dest_path
+            assert os.path.exists(dest_path)
+            assert os.stat(dest_path).st_size == 0
+    finally:
+        if os.path.exists(dest_path):
+            os.remove(dest_path)
+
+
+def test_fetch_partitioned_data_set_empty(ansible_zos_module):
+    hosts = ansible_zos_module
+    params = dict(
+        src='IMSTESTL.HWSRCDR',
+        dest='/tmp/',
+        flat=True    
+    )
+    results = hosts.all.zos_fetch(**params)
+    dest_path = '/tmp/IMSTESTL.HWSRCDR'
+    try:
+        for result in results.contacted.values():
+            assert result.get('changed') is True
+            assert result.get('data_set_type') == 'Partitioned'
+            assert result.get('module_stderr') is None
+            assert os.path.exists(dest_path)
+            assert os.path.isdir(dest_path)
+            assert len(next(os.walk(dest_path))[1]) == 0
+    finally:
+        if os.path.exists(dest_path):
+            shutil.rmtree(dest_path)
+
+
+def test_fetch_partitioned_data_set_member_empty(ansible_zos_module):
+    hosts = ansible_zos_module
+    pds_name = "ZOS.FETCH.TEST.PDS"
+    hosts.all.zos_data_set(
+        name=pds_name,
+        type='pds',
+        size='5M',
+        format='fba',
+        record_length=25
+    )
+    hosts.all.zos_data_set(
+        name=pds_name + "(MYDATA)",
+        type='MEMBER',
+        replace='yes'
+    )
+    params = dict(
+        src='ZOS.FETCH.TEST.PDS(MYDATA)',
+        dest='/tmp/',
+        flat=True    
+    )
+    results = hosts.all.zos_fetch(**params)
+    dest_path = '/tmp/ZOS.FETCH.TEST.PDS'
+    try:
+        for result in results.contacted.values():
+            assert result.get('changed') is True
+            assert result.get('data_set_type') == 'Partitioned'
+            assert result.get('module_stderr') is None
+            assert os.path.exists(dest_path)
+            assert os.path.isdir(dest_path)
+            assert os.path.exists(dest_path + "/MYDATA")
+            assert os.stat(dest_path + "/MYDATA").st_size == 0
+    finally:
+        if os.path.exists(dest_path):
+            shutil.rmtree(dest_path)
+        hosts.all.zos_data_set(name=pds_name, state='absent')
+
+
+def test_fetch_uss_file_continue_if_missing(ansible_zos_module):
+    hosts = ansible_zos_module
+    params = dict(
+        src='/tmp/dummy_file_on_remote_host',
+        dest='/tmp/',
+        flat=True,
+        fail_on_missing=False
+    )
+    results = hosts.all.zos_fetch(**params)
+    dest_path = '/tmp/profile'
+    try:
+        for result in results.contacted.values():
+            assert result.get('changed') is False
+            assert result.get('failed') is False
+            assert result.get('data_set_type') == 'Unix'
+            assert result.get('module_stderr') is None

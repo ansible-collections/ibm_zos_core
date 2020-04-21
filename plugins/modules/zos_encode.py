b7ac70294df0aba7a5b93a0796a44d5715aab0f6
yourfuwa2015 <16587467+yourfuwa2015@users.noreply.github.com>
2020-04-21 18:57:54 +0800

    doc refine

--- plugins/modules/zos_encode.py
+++ plugins/modules/zos_encode.py
@@ -15,16 +15,18 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 DOCUMENTATION = r'''
 module: zos_encode
 author: "Zhao Lu (@yourfuwa2015)"
-short_description: Convert the encoding of characters
+short_description: Convert the encoding of characters read from a USS file or
+                   an MVS data set.
 description:
-    - Converts the encoding of characters read from either Unix System Services
-      (USS), PS(sequential data set), PDS/E or KSDS(VSAM data set).
-    - Write the data out to either Unix System Services (USS),
+    - Convert the encoding of characters read from either Unix System
+      Services (USS) file or path, PS(sequential data set), PDS/E or KSDS(
+      VSAM data set).
+    - Write the data out to either Unix System Services (USS) file or path,
       PS(sequential data set), PDS/E or KSDS(VSAM data set).
 options:
   from_encoding:
     description:
-        - The encoding of the C(src).
+        - The source code set of the input(src).
         - Supported charsets rely on the target version, the most common
           charsets are supported.
     required: false
@@ -32,7 +34,7 @@ options:
     default: IBM-1047
   to_encoding:
     description:
-        - The encoding of the C(dest).
+        - The destination code set for the output(dest).
         - Supported charsets rely on the target version, the most common
           charsets are supported.
     required: false
@@ -40,33 +42,37 @@ options:
     default: ISO8859-1
   src:
     description:
-        - The location of the data residing in a USS file, USS
-          directory, PS(sequential data set), PDS/E or KSDS(VSAM data set)
-          to be encoded.
-        - If the source is a USS directory all files will be encoding it
+        - The location of the input characters.
+        - It could be a USS file, USS directory, PS(sequential data set),
+          PDS/E or KSDS(VSAM data set).
+        - The USS path or file must be an absolute pathname.
+        - If the source is a USS directory, all files will be encoding, it
           is to the user to avoid files that should not be encoded such as
           binary files
-        - The USS path or file must be an absolute pathname
     required: true
     type: str
   dest:
     description:
-        - This is the destination of where the data should be written to
-          after it has been encoded.
-        - If the C(dest) is not specified the C(src) will be overwritten with
-          the selected charset.
-        - This destination can be a USS file, USS path,
-          PS(sequential data set), PDS/E or KSDS(VSAM data set).
+        - The location of the coverted characters to be written out to.
+        - It could be a USS file, USS directory, PS(sequential data set),
+          PDS/E or KSDS(VSAM data set).
+        - If the dest is not specified, the src will be used. It will be
+          converted and overwritten with the specified charset in the
+          to_encoding.
         - If length of the file name in src is more the 8 characters, name
           will be truncated when converting to a PDS.
-        - The USS path or file must be an absolute pathname
+        - If src is a USS file, PS, VSAM or PDS/E member, a file will be 
+          created for the dest when no dest specified.
+        - If src is a USS path, PDS/E, a path will be created for the dest 
+          when no dest specified.
+        - The USS path or file must be an absolute pathname.
     required: false
     type: str
   backup:
     description:
-      - Create a backup file or backup data set including the timestamp
-        information so you can get the original file back if you somehow
-        clobbered it incorrectly.
+      - Create a backup file or backup data set for dest including the
+        timestamp information so you can get the original file back if you
+        somehow clobbered it incorrectly.
       - If the dest is a USS file or USS path, the name of the backup file
         will be the destination file or path name appended with a timestamp,
         e.g. /path/file_name.2020-04-23-08-32-29-bak.tar.
@@ -74,7 +80,7 @@ options:
         will be the MVS data set name appended with two qualifiers to
         indicate timestamp information,
         e.g. SOURCE.DATA.SET.NAME.D200423.T083229
-      - USS files or paths are backed up in a compressed format, the USS
+      - USS file or path are backuped in a compressed format, the USS
         pax or tar command is required for recovery.
       - MVS backup data set recovery can be done by renaming it.
     required: false
@@ -92,8 +98,8 @@ EXAMPLES = r'''
   zos_encode:
     src: /zos_encode/test.data
 
-- name: Convert file encoding from IBM-1047 to ISO8859-1 to another file
-    with backup
+- name: Convert file encoding from IBM-1047 to ISO8859-1 to another file with
+    backup
   zos_encode:
     src: /zos_encode/test.data
     dest: /zos_encode_out/test.out
@@ -204,14 +210,16 @@ src:
     returned: always
     type: str
 dest:
-    description: The name of the output file or data set
+    description: The name of the output file or data set, if the dest is a
+      uss file or path and the file status has been changed in the conversion,
+      the file stat info also will also be returned.
     returned: always
     type: str
 backup_file:
     description: Name of backup file created
     returned: changed and if backup=yes
     type: str
-    sample: /path/file_name.2020-04-23-08-32-29-bak.tar (if src is a USS file or path)
+    sample: /path/file_name.2020-04-23-08-32-29-bak.tar
 changed:
     description: True if the state was changed, otherwise False
     returned: always
@@ -441,7 +449,7 @@ def run_module():
         is_uss_dest, is_mvs_dest, ds_type_dest, err_msg = check_file(dest)
         if (not is_uss_dest) and (path.sep in dest):
             try:
-                if path.isfile(src):
+                if path.isfile(src) or ds_type_src in ['PS', 'VSAM']:
                     head, tail = path.split(dest)
                     if not path.exists(head):
                         makedirs(head)

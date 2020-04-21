a24f211e48cac96b8ce6f5903233cea3b392c50f
Blake Becker <blakejordanbecker@gmail.com>
2020-04-21 10:06:01 -0700

    added missing license and imports

--- plugins/module_utils/import_handler.py
+++ plugins/module_utils/import_handler.py
@@ -1,3 +1,11 @@
+# Copyright (c) IBM Corporation 2020
+# Apache License, Version 2.0 (see https://opensource.org/licenses/Apache-2.0)
+
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
 class MissingZOAUImport(object):
     def __getattr__(self, name):
         def method(*args, **kwargs):

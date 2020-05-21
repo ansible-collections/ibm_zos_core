f239279c32dffbaaa7b133c7612105bc8b938f65
balkajbaf <balkajbaf.ibm@gmail.com>
2020-05-20 19:16:00 -0700

    dataset test cases; create a dataset/file with content instead of being dependent on existing source

--- tests/functional/modules/test_zos_lineinfile_func.py
+++ tests/functional/modules/test_zos_lineinfile_func.py
@@ -6,110 +6,243 @@
 from __future__ import absolute_import, division, print_function
 import os
 import sys
+import pytest
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../helpers'))
-from test_zos_lineinfile_helper import test_uss_general
+from test_zos_lineinfile_helper import test_uss_general, test_ds_general
 
 __metaclass__ = type
 
+TEST_CONTENT = """
+if [ -z "$STEPLIB" ] && tty -s;
+then
+    export STEPLIB=none
+    exec -a $0 $SHELL
+fi
+TZ=PST8PDT
+export TZ
+LANG=C
+export LANG
+readonly LOGNAME
+PATH=/usr/lpp/zoautil/v100/bin:/usr/lpp/rsusr/ported/bin:/bin:/var/bin    
+export PATH
+LIBPATH=/usr/lpp/izoda/v110/anaconda/lib:/usr/lpp/zoautil/v100/lib:/lib
+export LIBPATH
+NLSPATH=/usr/lib/nls/msg/%L/%N
+export NLSPATH
+MANPATH=/usr/man/%L
+export MANPATH
+MAIL=/usr/mail/$LOGNAME
+export MAIL
+umask 022
+ZOAU_ROOT=/usr/lpp/zoautil/v100
+ZOAUTIL_DIR=/usr/lpp/zoautil/v100
+PYTHONPATH=/usr/lpp/izoda/v110/anaconda/lib:/usr/lpp/zoautil/v100/lib:/lib
+PKG_CONFIG_PATH=/usr/lpp/izoda/v110/anaconda/lib/pkgconfig
+PYTHON_HOME=/usr/lpp/izoda/v110/anaconda
+_BPXK_AUTOCVT=ON
+export ZOAU_ROOT
+export ZOAUTIL_DIR
+export ZOAUTIL_DIR
+export PYTHONPATH
+export PKG_CONFIG_PATH
+export PYTHON_HOME
+export _BPXK_AUTOCVT
+"""
+
+DS_TYPE = ['SEQ', 'PDS', 'PDSE']
+ENCODING = ['IBM-1047', 'ISO8859-1']
+
 TEST_ENV = dict(
-    USS_FILE = "/etc/profile",
+    TEST_CONT = TEST_CONTENT,
     TEST_DIR = "/tmp/zos_lineinfile/",
     TEST_FILE = "",
+    DS_NAME = "",
+    DS_TYPE = "",
+    ENCODING = "",
 )
 
 TEST_INFO = dict(
-    test_uss_line_replace = dict(path="", regexp="^ZOAU_ROOT=", line="ZOAU_ROOT=/mvsutil-develop_dsed", state="present"),
-    test_uss_line_insertafter_regex = dict(insertafter="^ZOAU_ROOT=", line="ZOAU_ROOT=/mvsutil-develop_dsed", state="present"),
-    test_uss_line_insertbefore_regex = dict(insertbefore="^ZOAU_ROOT=", line="unset ZOAU_ROOT", state="present"),
+    test_uss_line_replace = dict(path="", regexp="ZOAU_ROOT=", line="ZOAU_ROOT=/mvsutil-develop_dsed", state="present"),
+    test_uss_line_insertafter_regex = dict(insertafter="ZOAU_ROOT=", line="ZOAU_ROOT=/mvsutil-develop_dsed", state="present"),
+    test_uss_line_insertbefore_regex = dict(insertbefore="ZOAU_ROOT=", line="unset ZOAU_ROOT", state="present"),
     test_uss_line_insertafter_eof = dict(insertafter="EOF", line="export ZOAU_ROOT", state="present"),
     test_uss_line_insertbefore_bof = dict(insertbefore="BOF", line="# this is file is for setting env vars", state="present"),
-    test_uss_line_replace_match_insertafter_ignore = dict(regexp="^ZOAU_ROOT=", insertafter="^PATH=", 
+    test_uss_line_replace_match_insertafter_ignore = dict(regexp="ZOAU_ROOT=", insertafter="PATH=", 
                                             line="ZOAU_ROOT=/mvsutil-develop_dsed", state="present"),
-    test_uss_line_replace_match_insertbefore_ignore = dict(regexp="^ZOAU_ROOT=", insertbefore="^PATH=", line="unset ZOAU_ROOT", 
+    test_uss_line_replace_match_insertbefore_ignore = dict(regexp="ZOAU_ROOT=", insertbefore="PATH=", line="unset ZOAU_ROOT", 
                                             state="present"),
-    test_uss_line_replace_nomatch_insertafter_match = dict(regexp="abcxyz", insertafter="^ZOAU_ROOT=", 
+    test_uss_line_replace_nomatch_insertafter_match = dict(regexp="abcxyz", insertafter="ZOAU_ROOT=", 
                                             line="ZOAU_ROOT=/mvsutil-develop_dsed", state="present"),
-    test_uss_line_replace_nomatch_insertbefore_match = dict(regexp="abcxyz", insertbefore="^ZOAU_ROOT=", line="unset ZOAU_ROOT", 
+    test_uss_line_replace_nomatch_insertbefore_match = dict(regexp="abcxyz", insertbefore="ZOAU_ROOT=", line="unset ZOAU_ROOT", 
                                             state="present"),
     test_uss_line_replace_nomatch_insertafter_nomatch = dict(regexp="abcxyz", insertafter="xyzijk", 
                                             line="ZOAU_ROOT=/mvsutil-develop_dsed", state="present"),
     test_uss_line_replace_nomatch_insertbefore_nomatch = dict(regexp="abcxyz", insertbefore="xyzijk", line="unset ZOAU_ROOT", 
                                             state="present"),
-    test_uss_line_absent = dict(regexp="^ZOAU_ROOT=", state="absent"),
+    test_uss_line_absent = dict(regexp="ZOAU_ROOT=", state="absent"),
+    test_ds_line_replace = dict(test_name="T1"),
+    test_ds_line_insertafter_regex = dict(test_name="T2"),
+    test_ds_line_insertbefore_regex = dict(test_name="T3"),
+    test_ds_line_insertafter_eof = dict(test_name="T4"),
+    test_ds_line_insertbefore_bof = dict(test_name="T5"),
+    test_ds_line_replace_match_insertafter_ignore = dict(test_name="T6"),
+    test_ds_line_replace_match_insertbefore_ignore = dict(test_name="T7"),
+    test_ds_line_replace_nomatch_insertafter_match = dict(test_name="T8"),
+    test_ds_line_replace_nomatch_insertbefore_match = dict(test_name="T9"),
+    test_ds_line_replace_nomatch_insertafter_nomatch = dict(test_name="T10"),
+    test_ds_line_replace_nomatch_insertbefore_nomatch = dict(test_name="T11"),
+    test_ds_line_absent = dict(test_name="T12"),
 )
 
+#########################
+# USS test cases
+#########################
 
+@pytest.mark.uss
 def test_uss_line_replace(ansible_zos_module):
     test_uss_general("test_uss_line_replace", ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace"])
 
+@pytest.mark.uss
 def test_uss_line_insertafter_regex(ansible_zos_module):
     test_uss_general("test_uss_line_insertafter_regex", ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_insertafter_regex"])
 
+@pytest.mark.uss
 def test_uss_line_insertbefore_regex(ansible_zos_module):
     test_uss_general("test_uss_line_insertbefore_regex", ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_insertbefore_regex"])
 
+@pytest.mark.uss
 def test_uss_line_insertafter_eof(ansible_zos_module):
     test_uss_general("test_uss_line_insertafter_eof", ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_insertafter_eof"])
 
+@pytest.mark.uss
 def test_uss_line_insertbefore_bof(ansible_zos_module):
     test_uss_general("test_uss_line_insertbefore_bof", ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_insertbefore_bof"])
 
+@pytest.mark.uss
 def test_uss_line_replace_match_insertafter_ignore(ansible_zos_module):
     test_uss_general("test_uss_line_replace_match_insertafter_ignore", ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace_match_insertafter_ignore"])
 
+@pytest.mark.uss
 def test_uss_line_replace_match_insertbefore_ignore(ansible_zos_module):
     test_uss_general("test_uss_line_replace_match_insertbefore_ignore", ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace_match_insertbefore_ignore"])
 
+@pytest.mark.uss
 def test_uss_line_replace_nomatch_insertafter_match(ansible_zos_module):
     test_uss_general("test_uss_line_replace_nomatch_insertafter_match", ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace_nomatch_insertafter_match"])
 
+@pytest.mark.uss
 def test_uss_line_replace_nomatch_insertbefore_match(ansible_zos_module):
     test_uss_general("test_uss_line_replace_nomatch_insertbefore_match", ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace_nomatch_insertbefore_match"])
 
+@pytest.mark.uss
 def test_uss_line_replace_nomatch_insertafter_nomatch(ansible_zos_module):
     test_uss_general("test_uss_line_replace_nomatch_insertafter_nomatch", ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace_nomatch_insertafter_nomatch"])
 
+@pytest.mark.uss
 def test_uss_line_replace_nomatch_insertbefore_nomatch(ansible_zos_module):
     test_uss_general("test_uss_line_replace_nomatch_insertbefore_nomatch", ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace_nomatch_insertbefore_nomatch"])
 
+@pytest.mark.uss
 def test_uss_line_absent(ansible_zos_module):
     test_uss_general("test_uss_line_absent", ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_absent"])
 
-"""
-def test_ds_line_replace(ansible_zos_module):
-    test_ds_general("test_ds_line_replace", ansible_zos_module, TEST_ENV, TEST_INFO["test_ds_line_replace"])
+#########################
+# Dataset test cases
+#########################
 
-def test_ds_line_insertafter_regex(ansible_zos_module):
-    test_ds_general("test_ds_line_insertafter_regex", ansible_zos_module, TEST_ENV, TEST_INFO["test_ds_line_insertafter_regex"])
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+@pytest.mark.parametrize("encoding", ENCODING)
+def test_ds_line_replace(ansible_zos_module, dstype, encoding):
+    TEST_ENV["DS_TYPE"] = dstype
+    TEST_ENV["ENCODING"] = encoding
+    test_ds_general(TEST_INFO["test_ds_line_replace"]["test_name"], ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace"])
 
-def test_ds_line_insertbefore_regex(ansible_zos_module):
-    test_ds_general("test_ds_line_insertbefore_regex", ansible_zos_module, TEST_ENV, TEST_INFO["test_ds_line_insertbefore_regex"])
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+@pytest.mark.parametrize("encoding", ENCODING)
+def test_ds_line_insertafter_regex(ansible_zos_module, dstype, encoding):
+    TEST_ENV["DS_TYPE"] = dstype
+    TEST_ENV["ENCODING"] = encoding
+    test_ds_general(TEST_INFO["test_ds_line_insertafter_regex"]["test_name"], ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_insertafter_regex"])
 
-def test_ds_line_insertafter_eof(ansible_zos_module):
-    test_ds_general("test_ds_line_insertafter_eof", ansible_zos_module, TEST_ENV, TEST_INFO["test_ds_line_insertafter_eof"])
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+@pytest.mark.parametrize("encoding", ENCODING)
+def test_ds_line_insertbefore_regex(ansible_zos_module, dstype, encoding):
+    TEST_ENV["DS_TYPE"] = dstype
+    TEST_ENV["ENCODING"] = encoding
+    test_ds_general(TEST_INFO["test_ds_line_insertbefore_regex"]["test_name"], ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_insertbefore_regex"])
 
-def test_ds_line_insertbefore_bof(ansible_zos_module):
-    test_ds_general("test_ds_line_insertbefore_bof", ansible_zos_module, TEST_ENV, TEST_INFO["test_ds_line_insertbefore_bof"])
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+@pytest.mark.parametrize("encoding", ENCODING)
+def test_ds_line_insertafter_eof(ansible_zos_module, dstype, encoding):
+    TEST_ENV["DS_TYPE"] = dstype
+    TEST_ENV["ENCODING"] = encoding
+    test_ds_general(TEST_INFO["test_ds_line_insertafter_eof"]["test_name"], ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_insertafter_eof"])
 
-def test_ds_line_replace_match_insertafter_ignore(ansible_zos_module):
-    test_ds_general("test_ds_line_replace_match_insertafter_ignore", ansible_zos_module, TEST_ENV, TEST_INFO["test_ds_line_replace_match_insertafter_ignore"])
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+@pytest.mark.parametrize("encoding", ENCODING)
+def test_ds_line_insertbefore_bof(ansible_zos_module, dstype, encoding):
+    TEST_ENV["DS_TYPE"] = dstype
+    TEST_ENV["ENCODING"] = encoding
+    test_ds_general(TEST_INFO["test_ds_line_insertbefore_bof"]["test_name"], ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_insertbefore_bof"])
 
-def test_ds_line_replace_match_insertbefore_ignore(ansible_zos_module):
-    test_ds_general("test_ds_line_replace_match_insertbefore_ignore", ansible_zos_module, TEST_ENV, TEST_INFO["test_ds_line_replace_match_insertbefore_ignore"])
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+@pytest.mark.parametrize("encoding", ENCODING)
+def test_ds_line_replace_match_insertafter_ignore(ansible_zos_module, dstype, encoding):
+    TEST_ENV["DS_TYPE"] = dstype
+    TEST_ENV["ENCODING"] = encoding
+    test_ds_general(TEST_INFO["test_ds_line_replace_match_insertafter_ignore"]["test_name"], ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace_match_insertafter_ignore"])
 
-def test_ds_line_replace_nomatch_insertafter_match(ansible_zos_module):
-    test_ds_general("test_ds_line_replace_nomatch_insertafter_match", ansible_zos_module, TEST_ENV, TEST_INFO["test_ds_line_replace_nomatch_insertafter_match"])
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+@pytest.mark.parametrize("encoding", ENCODING)
+def test_ds_line_replace_match_insertbefore_ignore(ansible_zos_module, dstype, encoding):
+    TEST_ENV["DS_TYPE"] = dstype
+    TEST_ENV["ENCODING"] = encoding
+    test_ds_general(TEST_INFO["test_ds_line_replace_match_insertbefore_ignore"]["test_name"], ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace_match_insertbefore_ignore"])
 
-def test_ds_line_replace_nomatch_insertbefore_match(ansible_zos_module):
-    test_ds_general("test_ds_line_replace_nomatch_insertbefore_match", ansible_zos_module, TEST_ENV, TEST_INFO["test_ds_line_replace_nomatch_insertbefore_match"])
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+@pytest.mark.parametrize("encoding", ENCODING)
+def test_ds_line_replace_nomatch_insertafter_match(ansible_zos_module, dstype, encoding):
+    TEST_ENV["DS_TYPE"] = dstype
+    TEST_ENV["ENCODING"] = encoding
+    test_ds_general(TEST_INFO["test_ds_line_replace_nomatch_insertafter_match"]["test_name"], ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace_nomatch_insertafter_match"])
 
-def test_ds_line_replace_nomatch_insertafter_nomatch(ansible_zos_module):
-    test_ds_general("test_ds_line_replace_nomatch_insertafter_nomatch", ansible_zos_module, TEST_ENV, TEST_INFO["test_ds_line_replace_nomatch_insertafter_nomatch"])
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+@pytest.mark.parametrize("encoding", ENCODING)
+def test_ds_line_replace_nomatch_insertbefore_match(ansible_zos_module, dstype, encoding):
+    TEST_ENV["DS_TYPE"] = dstype
+    TEST_ENV["ENCODING"] = encoding
+    test_ds_general(TEST_INFO["test_ds_line_replace_nomatch_insertbefore_match"]["test_name"], ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace_nomatch_insertbefore_match"])
 
-def test_ds_line_replace_nomatch_insertbefore_nomatch(ansible_zos_module):
-    test_ds_general("test_ds_line_replace_nomatch_insertbefore_nomatch", ansible_zos_module, TEST_ENV, TEST_INFO["test_ds_line_replace_nomatch_insertbefore_nomatch"])
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+@pytest.mark.parametrize("encoding", ENCODING)
+def test_ds_line_replace_nomatch_insertafter_nomatch(ansible_zos_module, dstype, encoding):
+    TEST_ENV["DS_TYPE"] = dstype
+    TEST_ENV["ENCODING"] = encoding
+    test_ds_general(TEST_INFO["test_ds_line_replace_nomatch_insertafter_nomatch"]["test_name"], ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace_nomatch_insertafter_nomatch"])
 
-def test_ds_line_absent(ansible_zos_module):
-    test_ds_general("test_ds_line_absent", ansible_zos_module, TEST_ENV, TEST_INFO["test_ds_line_absent"])
-"""
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+@pytest.mark.parametrize("encoding", ENCODING)
+def test_ds_line_replace_nomatch_insertbefore_nomatch(ansible_zos_module, dstype, encoding):
+    TEST_ENV["DS_TYPE"] = dstype
+    TEST_ENV["ENCODING"] = encoding
+    test_ds_general(TEST_INFO["test_ds_line_replace_nomatch_insertbefore_nomatch"]["test_name"], ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_replace_nomatch_insertbefore_nomatch"])
+
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+@pytest.mark.parametrize("encoding", ENCODING)
+def test_ds_line_absent(ansible_zos_module, dstype, encoding):
+    TEST_ENV["DS_TYPE"] = dstype
+    TEST_ENV["ENCODING"] = encoding
+    test_ds_general(TEST_INFO["test_ds_line_absent"]["test_name"], ansible_zos_module, TEST_ENV, TEST_INFO["test_uss_line_absent"])
--- tests/helpers/test_zos_lineinfile_helper.py
+++ tests/helpers/test_zos_lineinfile_helper.py
@@ -5,32 +5,91 @@
 
 from __future__ import absolute_import, division, print_function
 import os
+from shellescape import quote
 from pprint import pprint
 
 __metaclass__ = type
 
 
 def set_uss_test_env(test_name, hosts, test_env):
-    test_env["TEST_FILE"] = test_env["TEST_DIR"] + test_env["USS_FILE"].split('/')[-1] + "." + test_name
+    test_env["TEST_FILE"] = test_env["TEST_DIR"] + test_name
     try:
         if not os.path.exists(test_env["TEST_DIR"]):
             os.mkdir(test_env["TEST_DIR"])
-        hosts.all.shell(cmd="cp -p {0} {1}".format(test_env["USS_FILE"], test_env["TEST_FILE"]))
+        hosts.all.shell(cmd="echo \"{0}\" > {1}".format(test_env["TEST_CONT"], test_env["TEST_FILE"]))
     except:
-        assert "Failed to set the test env"
+        assert 1==0, "Failed to set the test env"
 
-def clean_uss_test_env(test_dir):
+def clean_uss_test_env(test_dir, hosts):
     try:
-        os.rmdir(test_dir)
+        hosts.all.shell(cmd="rm -rf " + test_dir)
     except:
-        assert "Failed to clean the test env"
+        assert 1==0, "Failed to clean the test env"
 
 def test_uss_general(test_name, ansible_zos_module, test_env, test_info):
     hosts = ansible_zos_module
     set_uss_test_env(test_name, hosts, test_env)
     test_info["path"] = test_env["TEST_FILE"]
     results = hosts.all.zos_lineinfile(**test_info)
-    #clean_uss_test_env(test_env["TEST_DIR"])
+    clean_uss_test_env(test_env["TEST_DIR"], hosts)
+    pprint(vars(results))
+    for result in results.contacted.values():
+        assert result.get("changed") == 1
+
+def set_ds_test_env(test_name, hosts, test_env):
+    TEMP_FILE = "/tmp/" + test_name
+    encoding = test_env["ENCODING"].replace("-", "").replace(".", "").upper()
+    
+    try:
+        int(encoding[0])
+        encoding = "E" + encoding
+    except:
+        pass
+    if len(encoding) > 7:
+        encoding = encoding[:4] + encoding[-4:]
+
+    test_env["DS_NAME"] = test_name.upper() + "." + encoding + "." + test_env["DS_TYPE"]
+    cmdStr = "python -c \"from zoautil_py import Datasets; Datasets.create('" + test_env["DS_NAME"] + "', type='" + test_env["DS_TYPE"] + "')\" "
+
+    try:
+        hosts.all.shell(cmd=cmdStr)
+        hosts.all.shell(cmd="echo \"{0}\" > {1}".format(test_env["TEST_CONT"], TEMP_FILE))
+        if test_env["DS_TYPE"] in ["PDS", "PDSE"]:
+            test_env["DS_NAME"] =  test_env["DS_NAME"] + "(MEM)"
+            hosts.all.zos_data_set(name=test_env["DS_NAME"], state="present", type="member")
+            cmdStr = "cp -CM {0} \"//'{1}'\"".format(quote(TEMP_FILE), test_env["DS_NAME"])
+        else:
+            cmdStr = "cp {0} \"//'{1}'\" ".format(quote(TEMP_FILE), test_env["DS_NAME"])
+
+        if test_env["ENCODING"] != "IBM-1047":
+            hosts.all.zos_encode(src=TEMP_FILE, dest=test_env["DS_NAME"], from_encoding="IBM-1047", to_encoding=test_env["ENCODING"])
+        else:
+            hosts.all.shell(cmd=cmdStr)
+        hosts.all.shell(cmd="rm -rf " + TEMP_FILE)
+        cmdStr = "cat \"//'{0}'\" | wc -l ".format(test_env["DS_NAME"])
+        results = hosts.all.shell(cmd=cmdStr)
+        pprint(vars(results))
+        for result in results.contacted.values():
+            assert int(result.get("stdout")) != 0
+    except:
+        assert 1==0, "Failed to set the test env"
+
+def clean_ds_test_env(ds_name, hosts):
+    ds_name = ds_name.replace("(MEM)", "")
+    cmdStr = "python -c \"from zoautil_py import Datasets; Datasets.delete('" + ds_name + "')\" "
+    try:
+        hosts.all.shell(cmd=cmdStr)
+    except:
+        assert 1==0, "Failed to clean the test env"
+
+def test_ds_general(test_name, ansible_zos_module, test_env, test_info):
+    hosts = ansible_zos_module
+    set_ds_test_env(test_name, hosts, test_env)
+    test_info["path"] = test_env["DS_NAME"]
+    if test_env["ENCODING"]:
+        test_info["encoding"] = test_env["ENCODING"]
+    results = hosts.all.zos_lineinfile(**test_info)
+    clean_ds_test_env(test_env["DS_NAME"], hosts)
     pprint(vars(results))
     for result in results.contacted.values():
         assert result.get("changed") == 1

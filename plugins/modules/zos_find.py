61dc373d409eb775c7e78481918843fd34ab3ab3
Asif Mahmud <asif.mahmud@ibm.com>
2020-06-16 14:53:10 -0700

    Begin doc

---
+++ plugins/modules/zos_find.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Copyright (c) IBM Corporation 2020
+# Apache License, Version 2.0 (see https://opensource.org/licenses/Apache-2.0)
+
+from __future__ import (absolute_import, division, print_function)
+__metaclass__ = type
+
+ANSIBLE_METADATA = {'metadata_version': '1.1',
+                    'status': ['stableinterface'],
+                    'supported_by': 'community'}
+
+DOCUMENTATION = r"""
+---
+module: zos_find
+version_added: '2.9'
+short_description: Find matching data sets
+description:
+  - The M(zos_find) module 
+author: "Asif Mahmud (@asifmahmud)"
+options:
+  age:
+    - 
+  age_since:
+    -
+  
+
+
+"""
+
+
+EXAMPLES = r"""
+
+"""
+
+
+RETURN = r"""
+
+
+"""

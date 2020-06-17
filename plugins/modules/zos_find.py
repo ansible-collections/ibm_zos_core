6f201281205d1fe3bb14dd6e09dd0d9f66a80043
Asif Mahmud <asif.mahmud@ibm.com>
2020-06-17 16:16:52 -0700

    age and age_stamp has been documented

--- plugins/modules/zos_find.py
+++ plugins/modules/zos_find.py
@@ -16,13 +16,45 @@ module: zos_find
 version_added: '2.9'
 short_description: Find matching data sets
 description:
-  - The M(zos_find) module 
+  - Return a list of files or data sets based on specific criteria. 
+  - Multiple criteria are AND’d together.
 author: "Asif Mahmud (@asifmahmud)"
 options:
   age:
-    - 
-  age_since:
+    description:
+      - Select files or data sets whose age is equal to or greater than the specified time.
+      - Use a negative age to find data sets equal to or less than the specified time.
+      - You can choose seconds, minutes, hours, days, or weeks by specifying the 
+        first letter of any of those words (e.g., "1w").
+    type: str
+    required: false
+  age_stamp:
+    description:
+      - Choose the date property against which we compare age.
+      - C(c_date) refers to creation date and C(r_date) refers to referenced date.
+      - Only valid if C(age) is provided.
+    type: str
+    choices:
+      - c_date
+      - r_date
+    default: c_date
+    required: false
+  contains:
     -
+  exclude:
+    - 
+  get_checksum:
+    - 
+  patterns:
+    - 
+  size:
+    - 
+  use_regex:
+    - 
+  paths:
+    - 
+  file_type;
+    - 
   
 
 

44b0ee68c8622c9bf2bc1c6b44a69b5d24ffc320
ddimatos <dimatos@gmail.com>
2020-09-08 22:57:20 -0700

    Deprecate the script section that creates the united/single RST for playbooks

--- docs/scripts/auto-doc-gen.sh
+++ docs/scripts/auto-doc-gen.sh
@@ -16,40 +16,43 @@ DOC_DIR=`( cd "$SCRIPT_PATH" && pwd )| sed 's|\(.*\)/.*|\1|'`
 # Source the playbook*.rst into a single RST so that our topic is a single html
 ################################################################################
 
-# If playbooks-single.rst exists, zero it out so we generate the lastest based
-# on the payboook-*.rst files
-if [[ -f $DOC_DIR/source/playbooks-single.rst  ]]; then
-    :> $DOC_DIR/source/playbooks-single.rst
-fi
+################################################################################
+# Deprecated playbooks-single.rst
+# # If playbooks-single.rst exists, zero it out so we generate the lastest based
+# # on the payboook-*.rst files
+# if [[ -f $DOC_DIR/source/playbooks-single.rst  ]]; then
+#     :> $DOC_DIR/source/playbooks-single.rst
+# fi
 
-# Remove the first toctree entry and onwards from playbooks.rst to create a new
-# RST playbooks-single.rst. Note, you should ensure toctree entries are at the
-# end of the conent to not remove unwanted content.
-if [[ -f $DOC_DIR/source/playbooks.rst ]]; then
+# # Remove the first toctree entry and onwards from playbooks.rst to create a new
+# # RST playbooks-single.rst. Note, you should ensure toctree entries are at the
+# # end of the conent to not remove unwanted content.
+# if [[ -f $DOC_DIR/source/playbooks.rst ]]; then
 
-    # Remove the toctree entries
-    awk '/toctree/ {exit} {print}' $DOC_DIR/source/playbooks.rst >> $DOC_DIR/source/playbooks-single.rst
+#     # Remove the toctree entries
+#     awk '/toctree/ {exit} {print}' $DOC_DIR/source/playbooks.rst >> $DOC_DIR/source/playbooks-single.rst
 
-    # Concat the interested RSTs into playbooks-single.rst to create a single RST
-    if [[ -f $DOC_DIR/source/playbook_config_setup.rst && -f $DOC_DIR/source/playbook_inventory.rst && -f $DOC_DIR/source/playbook_group_vars.rst && -f $DOC_DIR/source/playbook_run.rst ]]; then
+#     # Concat the interested RSTs into playbooks-single.rst to create a single RST
+#     if [[ -f $DOC_DIR/source/playbook_config_setup.rst && -f $DOC_DIR/source/playbook_inventory.rst && -f $DOC_DIR/source/playbook_group_vars.rst && -f $DOC_DIR/source/playbook_run.rst ]]; then
 
-        # Inform readers this is auto generated thus avoding the need to maintain
-        echo ".. ...........................................................................\n$(<$DOC_DIR/source/playbooks-single.rst)" > $DOC_DIR/source/playbooks-single.rst
-        echo ".. Auto generated restructured text                                          .\n$(<$DOC_DIR/source/playbooks-single.rst)" > $DOC_DIR/source/playbooks-single.rst
-        echo ".. ...........................................................................\n$(<$DOC_DIR/source/playbooks-single.rst)" > $DOC_DIR/source/playbooks-single.rst
+#         # Inform readers this is auto generated thus avoding the need to maintain
+#         echo ".. ...........................................................................\n$(<$DOC_DIR/source/playbooks-single.rst)" > $DOC_DIR/source/playbooks-single.rst
+#         echo ".. Auto generated restructured text                                          .\n$(<$DOC_DIR/source/playbooks-single.rst)" > $DOC_DIR/source/playbooks-single.rst
+#         echo ".. ...........................................................................\n$(<$DOC_DIR/source/playbooks-single.rst)" > $DOC_DIR/source/playbooks-single.rst
 
-        # For each identified file we want to merge into playbooks-single.rst cat them and merge
-        for file in $DOC_DIR/source/playbook_config_setup.rst $DOC_DIR/source/playbook_inventory.rst $DOC_DIR/source/playbook_group_vars.rst $DOC_DIR/source/playbook_run.rst; do
-            echo "" >> $DOC_DIR/source/playbooks-single.rst;
-            cat "$file" >> $DOC_DIR/source/playbooks-single.rst;
-        done
-    else
-        # When unable to merge remove the auto generated RST so that is apparent
-        # it is not generated and will diff in a 'git status'
+#         # For each identified file we want to merge into playbooks-single.rst cat them and merge
+#         for file in $DOC_DIR/source/playbook_config_setup.rst $DOC_DIR/source/playbook_inventory.rst $DOC_DIR/source/playbook_group_vars.rst $DOC_DIR/source/playbook_run.rst; do
+#             echo "" >> $DOC_DIR/source/playbooks-single.rst;
+#             cat "$file" >> $DOC_DIR/source/playbooks-single.rst;
+#         done
+#     else
+#         # When unable to merge remove the auto generated RST so that is apparent
+#         # it is not generated and will diff in a 'git status'
 
-        rm -rf $DOC_DIR/source/playbooks-single.rst
-    fi
-fi
+#         rm -rf $DOC_DIR/source/playbooks-single.rst
+#     fi
+# fi
+################################################################################
 
 # If requirements-single.rst exists, zero it out so we generate the lastest based
 # on the requirements-*.rst files

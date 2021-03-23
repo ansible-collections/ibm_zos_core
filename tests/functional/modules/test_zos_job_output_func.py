d386b84cbe54e02d1110eec75df9a47f1272b3c1
Rich Parker <richp405@gmail.com>
2021-03-23 11:05:02 -0400

    corrected test to look for steps in ret_code, not in main return object

--- tests/functional/modules/test_zos_job_output_func.py
+++ tests/functional/modules/test_zos_job_output_func.py
@@ -86,5 +86,5 @@ def test_zos_job_output_job_exists(ansible_zos_module):
         print(result)
         assert result.get("changed") is False
         assert result.get("jobs") is not None
-        assert result.get("jobs")[0].get("steps") is not None
-        assert result.get("jobs")[0].get("steps")[0].get("stepid") == "STEP0001"
+        assert result.get("jobs")[0].get("ret_code").get("steps") is not None
+        assert result.get("jobs")[0].get("ret_code").get("steps")[0].get("stepid") == "STEP0001"

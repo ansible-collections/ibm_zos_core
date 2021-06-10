3b94ccb36dd28285363d2d3b414254352e1e120c
Ketan Kelkar <ktnklkr@gmail.com>
2021-06-09 17:47:37 -0700

    cleanup exploratory code snippets, improve var naming

--- plugins/module_utils/facts/zinfo_collector.py
+++ plugins/module_utils/facts/zinfo_collector.py
@@ -13,54 +13,47 @@ from ansible.module_utils.facts.collector import BaseFactCollector
 
 class ZinfoCollector(BaseFactCollector):
    # name and members of _fact_ids are valid entries for the gather_subset param
-   name = 'zos_c_test'
+   name = 'zos_test'
    _fact_ids = set()
 
    def collect(self, module, collected_facts):
 
-      zos_c_pgm_facts = {}
-      zos_c_pgm_facts['test_c_pgm'] = 'testing_zos_c'
+      zos_facts = {}
+      zos_facts['test_hard_coded_fact'] = 'i_am_a_hard_coded_fact'
+      # zos_other_facts = json.loads('{"test9" : "77" , "test8" : "44" }')
 
       cmd = "/u/oeusr01/zinfohelper"
       rc, fcinfo_out, err = module.run_command(cmd)
       # rc, fcinfo_out, err = module.run_command(cmd, use_unsafe_shell=True)
-      zos_c_pgm_facts['z_c_run'] = {
+      zos_facts['run_command'] = {
          'rc': rc,
          'fcinfo_out': fcinfo_out,
          'err': err
       }
 
-      print('hi')
-      print('type - ' + str(type(fcinfo_out)))
-      print(fcinfo_out)
-      print('bytes')
-      print(bytes(fcinfo_out, 'utf-8'))
-      print('bye')
-
-      zos_other_facts = json.loads('{"test9" : "77" , "test8" : "44" }')
-
-      zos_c_pgm_facts.update(zos_other_facts)
+   # TODO - implement actual error handling
 
       try:
-         print('try ------------------------- block')
-         # print(json.loads(fcinfo_out))
-         print(bytes(fcinfo_out, 'utf-8').hex())
-         # zos_zinfo = json.loads(fcinfo_out)
-      except JSONDecodeError as e:
+         iplinfo = json.loads(fcinfo_out.strip())
+         zos_facts['iplinfo'] = iplinfo
 
-         print('json decode error')
+      except Exception as e:
          print(e)
-         print('///')
-
          pass
-      except:
-         print("Errored out...")
-         pass
+         # try:
+         #    print(' ---- begin try block ---- ')
+         #    # print(json.loads(fcinfo_out))
+         #    # print(bytes(fcinfo_out, 'utf-8').hex())
+         #    zos_zinfo = json.loads(fcinfo_out.strip())
+         # except JSONDecodeError as e:
 
-      return zos_c_pgm_facts
+         #    print(' *** json decode error ***')
+         #    print(e)
+         #    print('///')
 
+         #    pass
+         # except:
+         #    print("Errored out...")
+         #    pass
 
-
-# 27 7b20 2274657374 22203a20372c20 227465737432 22203a2034207d 27
-
-# 7b20 5c 2274657374 5c 22203a20372c20 5c 227465737432 5c 22203a2034207d
+      return zos_facts
--- plugins/module_utils/facts/zos_collector.py
+++ plugins/module_utils/facts/zos_collector.py
@@ -18,34 +18,8 @@ class ZosFactCollector(BaseFactCollector):
 
     def collect(self, module, collected_facts):
         zos_test_facts = {}
-        zos_test_facts['test'] = 'testing'
+        zos_test_facts['test_base_collector_fact'] = 'testing-base'
 
-        # zos_test_facts['zos_aa_test1'] = 'testing-a1'
-        # zos_test_facts['zos_aa_test2'] = 'testing-a2'
-        # zos_test_facts['zos_aa_test3'] = 'testing-a3'
-        # zos_test_facts['zos_bb_test1'] = 'testing-b1'
-        # zos_test_facts['zos_bb_test2'] = 'testing-b2'
-        # zos_test_facts['zos_bb_test3'] = 'testing-b3'
-        # zos_test_facts['zos_bb_test4'] = 'testing-b4'
-        # zos_test_facts['zos_cc_test1'] = 'testing-c1'
-        # zos_test_facts['zos_cc_test2'] = 'testing-c2'
-
-        # cmd = 'sleep 5'
-        # rc, fcinfo_out, err = module.run_command(cmd)
-        # # rc, fcinfo_out, err = module.run_command(cmd, use_unsafe_shell=True)
-
-        # zos_test_facts['sleep50'] = "slept for 50"
-
-        # TODO - remove fully qualified path to opercmd (use path from env vars instead)
-        cmd = '/ZOAU/bin/opercmd D SYMBOLS'
-        rc, fcinfo_out, err = module.run_command(cmd)
-        # rc, fcinfo_out, err = module.run_command(cmd, use_unsafe_shell=True)
-
-        zos_test_facts['z_symbols'] = {
-            'rc': rc,
-            'fcinfo_out': fcinfo_out,
-            'err': err
-        }
 
         return zos_test_facts
 

1d70de7bb0f594480b882dec5b1dd23e114ffdcf
ddimatos <dimatos@gmail.com>
2021-07-07 22:44:49 -0700

    Update ingnore for 2.9 that will fail if it finds missing doc fragment in ansible 2.11
    Signed-off-by: ddimatos <dimatos@gmail.com>

--- plugins/connection/zos_ssh_2_11.py
+++ plugins/connection/zos_ssh_2_11.py
@@ -9,7 +9,7 @@ from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 DOCUMENTATION = '''
-    name: zos_ssh
+    name: ssh
     short_description: connect via ssh client binary
     description:
         - This connection plugin allows ansible to communicate to the target machines via normal ssh command

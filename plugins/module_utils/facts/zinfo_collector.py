cbc81d34e4eff8ee4c60ffd70aadd131dc39dfb8
Ketan Kelkar <ktnklkr@gmail.com>
2021-08-27 14:48:47 -0700

    add logic to collector to run zinfo on uss and parse output

--- plugins/module_utils/facts/zinfo_collector.py
+++ plugins/module_utils/facts/zinfo_collector.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import (absolute_import, division, print_function)
-from json.decoder import JSONDecodeError
 __metaclass__ = type
 
 
@@ -19,23 +18,30 @@ class ZinfoCollector(BaseFactCollector):
    def collect(self, module, collected_facts):
 
       zos_facts = {}
-      zos_facts['test_hard_coded_fact'] = 'i_am_a_hard_coded_fact'
+      # zos_facts['test_hard_coded_fact'] = 'i_am_a_hard_coded_fact'
       # zos_other_facts = json.loads('{"test9" : "77" , "test8" : "44" }')
+      # zstr = '{"test9" : "77" , "test8" : "44" }'
 
-      cmd = "/u/oeusr01/zinfohelper"
-      rc, fcinfo_out, err = module.run_command(cmd)
-      # rc, fcinfo_out, err = module.run_command(cmd, use_unsafe_shell=True)
-      zos_facts['run_command'] = {
-         'rc': rc,
-         'fcinfo_out': fcinfo_out,
-         'err': err
-      }
+      # TODO - add logic here to figure out which call to zinfo to make
+      # zinfo -t aaa -t bbb -t ccc ... vs zinfo -a
+      # Maybe just a combination of '-t's for now (and not ever call -a)
+
+
+      cmd = "/u/oeusr01/zinfohelper -t ipl -t exp"
+      rc, fcinfo_out, err = module.run_command(cmd, encoding=None)
+      decode_str = fcinfo_out.decode('utf-8')
+
+      # zos_facts['run_command'] = {
+      #    'rc': rc,
+      #    'fcinfo_out': fcinfo_out,
+      #    'err': err
+      # }
 
    # TODO - implement actual error handling
 
       try:
-         iplinfo = json.loads(fcinfo_out.strip())
-         zos_facts['iplinfo'] = iplinfo
+
+         zos_facts['zinfo'] = json.loads(decode_str)
 
       except Exception as e:
          print(e)

7718e3025bd9b16d24fec9d5a7acce945b1b7957
Rich Parker <richp405@gmail.com>
2022-04-22 15:25:17 -0400

    tweaking jobs.py importer for zoau stuff

--- plugins/module_utils/job.py
+++ plugins/module_utils/job.py
@@ -23,7 +23,13 @@ from ansible_collections.ibm.ibm_zos_core.plugins.module_utils.better_arg_parser
 from ansible_collections.ibm.ibm_zos_core.plugins.module_utils.ansible_module import (
     AnsibleModuleHelper,
 )
-from zoautil_py.jobs import listing, read_output, list_dds
+# from zoautil_py.jobs import listing, read_output, list_dds
+try:
+    from zoautil_py.jobs import listing, read_output, list_dds
+except Exception:
+    listing = MissingZOAUImport()
+    read_output = MissingZOAUImport()
+    list_dds = MissingZOAUImport()
 
 
 def job_output(job_id=None, owner=None, job_name=None, dd_name=None):

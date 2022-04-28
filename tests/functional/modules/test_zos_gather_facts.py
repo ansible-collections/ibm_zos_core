24e234b277de28d33f32e70ed87cc41f41c147ba
Ketan Kelkar <ktnklkr@gmail.com>
2022-04-28 16:10:57 -0700

    add tests around bad or malicious input

--- tests/functional/modules/test_zos_gather_facts.py
+++ tests/functional/modules/test_zos_gather_facts.py
@@ -18,8 +18,6 @@ import sys
 from unittest import result
 import pytest
 
-from pprint import pprint
-
 __metaclass__ = type
 
 def test_gather_facts(ansible_zos_module):
@@ -27,7 +25,6 @@ def test_gather_facts(ansible_zos_module):
     hosts = ansible_zos_module
     results = hosts.all.zos_gather_facts()
     for result in results.contacted.values():
-        print(result)
         assert result is not None
         # something was returned -- most basic test case.
 
@@ -38,6 +35,16 @@ def test_gather_facts_with_gather_subset(ansible_zos_module):
 
     for result in results.contacted.values():
         assert result is not None
+        # check certain known keys in ipl subset are present
+        assert "ieasym_card" in result.get('ansible_facts').keys()
+        assert "ipaloadxx" in result.get('ansible_facts').keys()
+        assert "master_catalog_dsn" in result.get('ansible_facts').keys()
+
+        # check certain known keys not in ipl subset are not present
+        assert "hw_name" not in result.get('ansible_facts').keys()
+        assert "iodf_name" not in result.get('ansible_facts').keys()
+        assert "cpc_nd_manufacturer" not in result.get('ansible_facts').keys()
+
 
 # test with filter=name and no gather_subset
 def test_gather_facts_with_filter(ansible_zos_module):
@@ -81,22 +88,73 @@ def test_gather_facts_with_subset_and_filter(ansible_zos_module):
         # sys_name is not in the iodf subset.
         assert "sys_name" not in result.get('ansible_facts').keys()
 
+def test_gather_facts_with_filter_asterisk(ansible_zos_module):
+
+    subset = ['ipl'] # to cut down on list of facts returned
+
+    hosts = ansible_zos_module
+    results = hosts.all.zos_gather_facts(gather_subset=subset, filter=['*'])
+    for result in results.contacted.values():
+        # known facts in ipl subset should still be in ansible_facts
+        assert "ieasym_card" in result.get('ansible_facts').keys()
+        assert "ipaloadxx" in result.get('ansible_facts').keys()
+        assert "master_catalog_dsn" in result.get('ansible_facts').keys()
 
 # erroneous output:
-# ansible handles wrong format so we can expect gather_subset and filter to always be lists of str.
+# ansible handles input in wrong format so we can expect gather_subset and filter to always be lists of str.
 
-# # bad subsets -
-#     results = hosts.all.zos_gather_facts(gather_subset=['']) # empty string
-#     results = hosts.all.zos_gather_facts(gather_subset=['    ']) # space chars
-#     results = hosts.all.zos_gather_facts(gather_subset=['asdfasdf']) # nonsense
-#     # space and other good chars
-#     results = hosts.all.zos_gather_facts(gather_subset=['   ipl'])
-#     # space and other bad chars
-#     results = hosts.all.zos_gather_facts(gather_subset=['   asdf '])
+# bad subsets -
 
-# # bad filters -
-#     # '*' should be same output as default
-#     results = hosts.all.zos_gather_facts(filter=['*'])
-#     results = hosts.all.zos_gather_facts(filter=['']) # empty
-#     results = hosts.all.zos_gather_facts(filter=[' ']) # space
-#     results = hosts.all.zos_gather_facts(filter=['asdfasdf']) # nonsense
+# nonsense subset
+test_data =[
+    (['   asdf']),
+    (['asdfasdf']),
+]
+@pytest.mark.parametrize("gather_subset", test_data)
+def test_gather_facts_with_gather_subset_bad(ansible_zos_module, gather_subset):
+    hosts = ansible_zos_module
+    results = hosts.all.zos_gather_facts(gather_subset=gather_subset)
+    for result in results.contacted.values():
+        assert result.get('failed') is True
+        assert result.get('zinfo_err_msg') is not None
+
+# attempted injection through subset
+def test_gather_facts_with_gather_subset_injection(ansible_zos_module):
+    hosts = ansible_zos_module
+    results = hosts.all.zos_gather_facts(gather_subset=['ipl; cat /.bashrc'])
+    for result in results.contacted.values():
+        assert result.get('failed') is True
+
+# whitespace subsets
+test_data =[
+    (['   ']),
+    (['']),
+    (["\t"]),
+]
+@pytest.mark.parametrize("gather_subset", test_data)
+def test_gather_facts_with_gather_subset_empty_str(ansible_zos_module, gather_subset):
+    hosts = ansible_zos_module
+    results = hosts.all.zos_gather_facts(gather_subset=gather_subset)
+    for result in results.contacted.values():
+        assert result.get('failed') is True
+
+# bad filters -
+test_data =[
+    ([' ']), # space
+    (['    ']), # spaces
+    (['']), # empty str
+    (['asdfasdf']) # nonsense
+]
+@pytest.mark.parametrize("filter_list", test_data)
+def test_gather_facts_with_bad_filter(ansible_zos_module, filter_list):
+
+    subset = ['ipl']
+
+    hosts = ansible_zos_module
+    results = hosts.all.zos_gather_facts(gather_subset=subset, filter=filter_list)
+    for result in results.contacted.values():
+        # known facts in ipl subset should not be in ansible_facts
+        assert "ieasym_card" not in result.get('ansible_facts').keys()
+        assert "ipaloadxx" not in result.get('ansible_facts').keys()
+        assert "master_catalog_dsn" not in result.get('ansible_facts').keys()
+

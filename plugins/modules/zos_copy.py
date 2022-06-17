0023590954862bc03c05373480b85ba4c5b116c5
Fernando Flores <fflores10@uabc.edu.mx>
2022-06-17 13:24:54 -0500

    Gh issue 329 ddname (#333)
    * Fixed typo for dd_name that prevented job output filtering based on dd_name
    
    * Added functional tests to verify ddname filtering
    
    * Update Readme to reflect supported ansible version
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Upate release note to reflect patched version 1.3.x
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Update galaxy.yml with added build_ignore and authors
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Update artifacts to correctly reflect execution environments
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Update module with boolean bug fix and version released
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Update modules docs and version released
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Update error message for clarity
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Update sanity tests to remove a doc reference
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * First commit for changelog support and backported release fragemetns
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Update changelog fragments because of yamlint blank lines
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * update zos_fetch test case to remove testing the sftp port which is deprecated
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * update test case with verbosity to troubleshoot file count
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * update test case with verbosity to troubleshoot file count
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Update the test case to add 1 to the count for the added profile
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Correct missing whitespace around arithmetic operator
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Chaning src file because I think there is a race condition where its used elsewhere
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Chaning src file because I think there is a race condition where its used elsewhere
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * add more debug stmt's to the test case
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Added more debug to the test
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * Update test case with more debug
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    * udpate test case to correctly validate copied file
    
    Signed-off-by: ddimatos <dimatos@gmail.com>
    
    Co-authored-by: Oscar Fernando Flores Garcia <fernando.flores@ibm.com>
    Co-authored-by: ddimatos <dimatos@gmail.com>

--- plugins/modules/zos_copy.py
+++ plugins/modules/zos_copy.py
@@ -20,12 +20,12 @@ __metaclass__ = type
 DOCUMENTATION = r"""
 ---
 module: zos_copy
-version_added: '1.2.0'
+version_added: '1.0.0'
 short_description: Copy data to z/OS
 description:
-  - The M(zos_copy) module copies a file or data set from a local or a
+  - The M(ibm.ibm_zos_core.zos_copy) module copies a file or data set from a local or a
     remote machine to a location on the remote machine.
-  - Use the M(zos_fetch) module to copy files or data sets from remote
+  - Use the M(ibm.ibm_zos_core.zos_fetch) module to copy files or data sets from remote
     locations to the local machine.
 author:
   - "Asif Mahmud (@asifmahmud)"
@@ -143,7 +143,6 @@ options:
     type: bool
     required: false
     default: false
-    version_added: "1.4.0"
   is_binary:
     description:
       - If set to C(true), indicates that the file or data set to be copied is a
@@ -185,12 +184,12 @@ options:
     required: false
   sftp_port:
     description:
-      - Configuring the SFTP port used by the M(zos_copy) module has been
+      - Configuring the SFTP port used by the M(ibm.ibm_zos_core.zos_copy) module has been
         deprecated and will be removed in ibm.ibm_zos_core collection version
         1.5.0.
       - Configuring the SFTP port with I(sftp_port) will no longer have any
         effect on which port is used by this module.
-      - To configure the SFTP port used for module M(zos_copy), refer to topic
+      - To configure the SFTP port used for module M(ibm.ibm_zos_core.zos_copy), refer to topic
         L(using connection plugins,https://docs.ansible.com/ansible/latest/plugins/connection.html#using-connection-plugins)
       - If C(ansible_port) is not specified, port 22 will be used.
     type: int
@@ -329,7 +328,7 @@ notes:
     - VSAM data sets can only be copied to other VSAM data sets.
     - For supported character sets used to encode data, refer to the
       L(documentation,https://ibm.github.io/z_ansible_collections_doc/ibm_zos_core/docs/source/resources/character_set.html).
-    - M(zos_copy) uses SFTP (Secure File Transfer Protocol) for the underlying
+    - M(ibm.ibm_zos_core.zos_copy) uses SFTP (Secure File Transfer Protocol) for the underlying
       transfer protocol; Co:Z SFTP is not supported. In the case of Co:z SFTP,
       you can exempt the Ansible userid on z/OS from using Co:Z thus falling back
       to using standard SFTP.

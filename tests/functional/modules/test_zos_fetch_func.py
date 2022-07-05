3600e0d63d81f2d1377c691a2c8d58118f797608
Oscar Fernando Flores Garcia <fernandofloresdev@gmail.com>
2022-07-05 12:55:13 -0500

    Modified test to use zos_copuy

--- tests/functional/modules/test_zos_fetch_func.py
+++ tests/functional/modules/test_zos_fetch_func.py
@@ -217,7 +217,7 @@ def test_fetch_vsam_data_set(ansible_zos_module):
             assert result.get("module_stderr") is None
             assert result.get("dest") == dest_path
             assert os.path.exists(dest_path)
-            file = open(dest_path, 'rb')
+            file = open(dest_path, 'r')
             assert file.read() == TEST_DATA
 
     finally:

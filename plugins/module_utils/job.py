b92d765545acfed6d24f01c1e198aef1eccf24fd
Rich Parker <richp405@gmail.com>
2022-07-08 17:06:21 -0400

    saved the job.py file with the search change needed.

--- plugins/module_utils/job.py
+++ plugins/module_utils/job.py
@@ -182,7 +182,7 @@ def _zget_job_status(job_id="*", owner="*", job_name="*"):
     else:
         job_query = job_id
 
-    entries = listing(job_query, owner)
+    entries = listing(job_query)
 
     final_entries = []
     if entries:

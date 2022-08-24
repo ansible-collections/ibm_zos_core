c80ff0c17a53491759f5caea85e7febda723f94f
Oscar Fernando Flores Garcia <fernandofloresdev@gmail.com>
2022-08-24 15:45:27 -0500

    Corrected pep8 issues

--- tests/functional/modules/test_zos_apf_func.py
+++ tests/functional/modules/test_zos_apf_func.py
@@ -301,7 +301,7 @@ def test_add_already_present(ansible_zos_module):
     results = hosts.all.zos_apf(**test_info)
     pprint(vars(results))
     for result in results.contacted.values():
-        # Return code 16 if ZOAU < 1.2.0 and RC is 8 if ZOAU >= 1.2.0 
+        # Return code 16 if ZOAU < 1.2.0 and RC is 8 if ZOAU >= 1.2.0
         assert result.get("rc") == 16 or result.get("rc") == 8
     test_info['state'] = 'absent'
     hosts.all.zos_apf(**test_info)
@@ -316,7 +316,7 @@ def test_del_not_present(ansible_zos_module):
     results = hosts.all.zos_apf(**test_info)
     pprint(vars(results))
     for result in results.contacted.values():
-        # Return code 16 if ZOAU < 1.2.0 and RC is 8 if ZOAU >= 1.2.0 
+        # Return code 16 if ZOAU < 1.2.0 and RC is 8 if ZOAU >= 1.2.0
         assert result.get("rc") == 16 or result.get("rc") == 8
     clean_test_env(hosts, test_info)
 
@@ -328,7 +328,7 @@ def test_add_not_found(ansible_zos_module):
     results = hosts.all.zos_apf(**test_info)
     pprint(vars(results))
     for result in results.contacted.values():
-        # Return code 16 if ZOAU < 1.2.0 and RC is 8 if ZOAU >= 1.2.0 
+        # Return code 16 if ZOAU < 1.2.0 and RC is 8 if ZOAU >= 1.2.0
         assert result.get("rc") == 16 or result.get("rc") == 8
 
 
@@ -341,7 +341,7 @@ def test_add_with_wrong_volume(ansible_zos_module):
     results = hosts.all.zos_apf(**test_info)
     pprint(vars(results))
     for result in results.contacted.values():
-        # Return code 16 if ZOAU < 1.2.0 and RC is 8 if ZOAU >= 1.2.0 
+        # Return code 16 if ZOAU < 1.2.0 and RC is 8 if ZOAU >= 1.2.0
         assert result.get("rc") == 16 or result.get("rc") == 8
     clean_test_env(hosts, test_info)
 

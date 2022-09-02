d2d41a6dd0f92fa8c4f931dfca95c06913911412
ddimatos <dimatos@gmail.com>
2022-09-02 11:38:35 -0700

    Adding back the added arg to fail_json to propagate the zinfo err
    Signed-off-by: ddimatos <dimatos@gmail.com>

--- plugins/modules/zos_gather_facts.py
+++ plugins/modules/zos_gather_facts.py
@@ -240,7 +240,7 @@ def run_module():
             err_msg = ('An invalid subset was detected. See \'zinfo_err_msg\' '
                        'for additional details.')
 
-        module.fail_json(msg=err_msg, **result)
+        module.fail_json(msg=err_msg, zinfo_err_msg=err)
 
     zinfo_dict = {}  # to track parsed zinfo facts.
 

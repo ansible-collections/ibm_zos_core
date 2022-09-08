c745d0a82552936f13c614458e69dfe4debd9668
Fernando Flores <fernandofloresdev@gmail.com>
2022-09-08 13:03:22 -0500

    Enhancement/9108/zos_job_output_test_files Modify zos_job_output functional test suite to not leave tmp files (#368)
    * Added try - finally blocks for cleanup
    
    * Removed unused import
    
    * Changed tmp path to tmp/jcl

--- tests/functional/modules/test_zos_job_output_func.py
+++ tests/functional/modules/test_zos_job_output_func.py
@@ -30,7 +30,7 @@ HELLO, WORLD
 //
 """
 
-TEMP_PATH = "/tmp/ansible/jcl"
+TEMP_PATH = "/tmp/jcl"
 
 
 def test_zos_job_output_no_job_id(ansible_zos_module):
@@ -108,64 +108,69 @@ def test_zos_job_output_reject(ansible_zos_module):
 
 
 def test_zos_job_output_job_exists(ansible_zos_module):
-    # adding verification that at least 1 step was returned
-    hosts = ansible_zos_module
-    hosts.all.file(path=TEMP_PATH, state="directory")
-    hosts.all.shell(
-        cmd="echo {0} > {1}/SAMPLE".format(quote(JCL_FILE_CONTENTS), TEMP_PATH)
-    )
+    try:
+        # adding verification that at least 1 step was returned
+        hosts = ansible_zos_module
+        hosts.all.file(path=TEMP_PATH, state="directory")
+        hosts.all.shell(
+            cmd="echo {0} > {1}/SAMPLE".format(quote(JCL_FILE_CONTENTS), TEMP_PATH)
+        )
 
-    jobs = hosts.all.zos_job_submit(
-        src="{0}/SAMPLE".format(TEMP_PATH), location="USS", wait=True, volume=None
-    )
+        jobs = hosts.all.zos_job_submit(
+            src="{0}/SAMPLE".format(TEMP_PATH), location="USS", wait=True, volume=None
+        )
 
-    for job in jobs.contacted.values():
-        assert job.get("jobs") is not None, "ASSERTION-FAILURE: jobs is empty"
+        for job in jobs.contacted.values():
+            assert job.get("jobs") is not None, "ASSERTION-FAILURE: jobs is empty"
 
-    for job in jobs.contacted.values():
-        submitted_job_id = job.get("jobs")[0].get("job_id")
-        print(submitted_job_id)
-        assert submitted_job_id is not None, "ASSERTION-FAILURE: jobs id is None"
+        for job in jobs.contacted.values():
+            submitted_job_id = job.get("jobs")[0].get("job_id")
+            print(submitted_job_id)
+            assert submitted_job_id is not None, "ASSERTION-FAILURE: jobs id is None"
 
-    hosts.all.file(path=TEMP_PATH, state="absent")
-    results = hosts.all.zos_job_output(job_id=submitted_job_id)  # was SAMPLE?!
-    for result in results.contacted.values():
-        assert result.get("changed") is False
-        assert result.get("jobs") is not None,\
-            "ASSERTION-FAILURE: jobs is empty, jobs = [{0}]".format(
-                result.get('jobs'))
-        print(result.get('jobs'))
-        assert result.get("jobs")[0].get("ret_code").get("steps") is not None,\
-            "ASSERTION-FAILURE: job steps empty, steps = [{0}]".format(
-                result.get('jobs')[0].get('ret_code').get('steps')[0])
-        print(result.get('jobs')[0].get('ret_code').get('steps')[0])
-        assert result.get("jobs")[0].get("ret_code").get("steps")[0].get("step_name") == "STEP0001", \
-            "ASSERTION-FAILURE: job steps empty, steps = [{0}]".format(
-                result.get('jobs')[0].get('ret_code').get('steps')[0].get('step_name'))
-        print(result.get('jobs')[0].get('ret_code').get(
-            'steps')[0].get('step_name'))
+        results = hosts.all.zos_job_output(job_id=submitted_job_id)  # was SAMPLE?!
+        for result in results.contacted.values():
+            assert result.get("changed") is False
+            assert result.get("jobs") is not None,\
+                "ASSERTION-FAILURE: jobs is empty, jobs = [{0}]".format(
+                    result.get('jobs'))
+            print(result.get('jobs'))
+            assert result.get("jobs")[0].get("ret_code").get("steps") is not None,\
+                "ASSERTION-FAILURE: job steps empty, steps = [{0}]".format(
+                    result.get('jobs')[0].get('ret_code').get('steps')[0])
+            print(result.get('jobs')[0].get('ret_code').get('steps')[0])
+            assert result.get("jobs")[0].get("ret_code").get("steps")[0].get("step_name") == "STEP0001", \
+                "ASSERTION-FAILURE: job steps empty, steps = [{0}]".format(
+                    result.get('jobs')[0].get('ret_code').get('steps')[0].get('step_name'))
+            print(result.get('jobs')[0].get('ret_code').get(
+                'steps')[0].get('step_name'))
+    finally:
+        hosts.all.file(path=TEMP_PATH, state="absent")
 
 
 def test_zos_job_output_job_exists_with_filtered_ddname(ansible_zos_module):
-    hosts = ansible_zos_module
-    hosts.all.file(path=TEMP_PATH, state="directory")
-    hosts.all.shell(
-        cmd="echo {0} > {1}/SAMPLE".format(quote(JCL_FILE_CONTENTS), TEMP_PATH)
-    )
-    hosts.all.zos_job_submit(
-        src="{0}/SAMPLE".format(TEMP_PATH), location="USS", wait=True, volume=None
-    )
-    hosts.all.file(path=TEMP_PATH, state="absent")
-    dd_name = "JESMSGLG"
-    results = hosts.all.zos_job_output(job_name="HELLO", ddname=dd_name)
-    for result in results.contacted.values():
-        assert result.get("changed") is False
-        assert result.get("jobs") is not None
-        for job in result.get("jobs"):
-            assert len(job.get("ddnames")) == 1,\
-                "ASSERTION-FAILURE: ddname count = [{0}]"\
-                .format(job.get("ddnames"))
-            print(job)
-            assert job.get("ddnames")[0].get("ddname") == dd_name,\
-                "ASSERTION-FAILURE: ddname name = [{0}]".\
-                format(job.get("ddnames")[0].get("ddname"))
+    try:
+        hosts = ansible_zos_module
+        hosts.all.file(path=TEMP_PATH, state="directory")
+        hosts.all.shell(
+            cmd="echo {0} > {1}/SAMPLE".format(quote(JCL_FILE_CONTENTS), TEMP_PATH)
+        )
+        hosts.all.zos_job_submit(
+            src="{0}/SAMPLE".format(TEMP_PATH), location="USS", wait=True, volume=None
+        )
+        hosts.all.file(path=TEMP_PATH, state="absent")
+        dd_name = "JESMSGLG"
+        results = hosts.all.zos_job_output(job_name="HELLO", ddname=dd_name)
+        for result in results.contacted.values():
+            assert result.get("changed") is False
+            assert result.get("jobs") is not None
+            for job in result.get("jobs"):
+                assert len(job.get("ddnames")) == 1,\
+                    "ASSERTION-FAILURE: ddname count = [{0}]"\
+                    .format(job.get("ddnames"))
+                print(job)
+                assert job.get("ddnames")[0].get("ddname") == dd_name,\
+                    "ASSERTION-FAILURE: ddname name = [{0}]".\
+                    format(job.get("ddnames")[0].get("ddname"))
+    finally:
+        hosts.all.file(path=TEMP_PATH, state="absent")

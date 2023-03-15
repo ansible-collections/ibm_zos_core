4005b6fc6e7a2a939cf43eda7ba235808166268d
Andre Marcel Gutierrez Benitez <andre@MacBook-Pro-de-Andre-2.local>
2023-03-15 15:12:08 -0600

    Verify coomand of ZOAU support the doble quotes and get better output of message

--- plugins/modules/zos_blockinfile.py
+++ plugins/modules/zos_blockinfile.py
@@ -567,6 +567,9 @@ def main():
         # Try to extract information from stdout
         ret = json.loads(stdout)
         ret['cmd'] = ret['cmd'].replace("u'",'"')
+        ret['changed'] = ret['changed'].replace("u'",'"')
+        ret['found'] = ret['found'].replace("u'",'"')
+
         result['cmd'] = ret['cmd']
         result['changed'] = ret['changed']
         result['found'] = ret['found']

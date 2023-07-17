388ec80f4a83db6744a69c74ade14200f7204997
Rich Parker <richp405@gmail.com>
2023-07-17 15:17:47 -0400

    Removed emergency backup and auto-recovery features. Initial changelog fragment.

--- plugins/modules/zos_copy.py
+++ plugins/modules/zos_copy.py
@@ -2609,32 +2609,6 @@ def run_module(module, arg_def):
             dest=dest
         )
 
-    # Creating an emergency backup or an empty data set to use as a model to
-    # be able to restore the destination in case the copy fails.
-    emergency_backup = ""
-    if dest_exists and not force:
-        if is_uss or not data_set.DataSet.is_empty(dest_name):
-            use_backup = True
-            if is_uss:
-                # When copying a directory without a trailing slash,
-                # appending the source's base name to the backup path to
-                # avoid backing up the whole parent directory that won't
-                # be modified.
-                src_basename = os.path.basename(src) if src else ''
-                backup_dest = "{0}/{1}".format(dest, src_basename) if is_src_dir and not src.endswith("/") else dest
-                backup_dest = os.path.normpath(backup_dest)
-                emergency_backup = tempfile.mkdtemp()
-                emergency_backup = backup_data(backup_dest, dest_ds_type, emergency_backup, tmphlq)
-            else:
-                if not (dest_ds_type in data_set.DataSet.MVS_PARTITIONED and src_member and not dest_member_exists):
-                    emergency_backup = backup_data(dest, dest_ds_type, None, tmphlq)
-        # If dest is an empty data set, instead create a data set to
-        # use as a model when restoring.
-        else:
-            use_backup = False
-            emergency_backup = data_set.DataSet.temp_name()
-            data_set.DataSet.allocate_model_data_set(emergency_backup, dest_name)
-
     # Here we'll use the normalized source file by shadowing the
     # original one. This change applies only to the
     # allocate_destination_data_set call.
@@ -2659,9 +2633,6 @@ def run_module(module, arg_def):
                 volume=volume
             )
     except Exception as err:
-        if dest_exists and not force:
-            restore_backup(dest_name, emergency_backup, dest_ds_type, use_backup)
-            erase_backup(emergency_backup, dest_ds_type)
         if converted_src:
             if remote_src:
                 src = original_src
@@ -2790,12 +2761,7 @@ def run_module(module, arg_def):
             res_args["changed"] = True
 
     except CopyOperationError as err:
-        if dest_exists and not force:
-            restore_backup(dest_name, emergency_backup, dest_ds_type, use_backup)
         raise err
-    finally:
-        if dest_exists and not force:
-            erase_backup(emergency_backup, dest_ds_type)
 
     res_args.update(
         dict(

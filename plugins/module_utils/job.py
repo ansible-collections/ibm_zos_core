d41f438e52fea1be692e4413efed65b1d074b53d
Rich Parker <richp405@gmail.com>
2023-10-16 10:58:31 -0400

    main change to version checker for consolidation

--- plugins/module_utils/job.py
+++ plugins/module_utils/job.py
@@ -31,10 +31,9 @@ except Exception:
     list_dds = MissingZOAUImport()
     listing = MissingZOAUImport()
 
-try:
-    from zoautil_py import ZOAU_API_VERSION
-except Exception:
-    ZOAU_API_VERSION = "1.2.0"
+from ansible_collections.ibm.ibm_zos_core.plugins.module_utils import (
+    zoau_version_checker
+)
 
 
 def job_output(job_id=None, owner=None, job_name=None, dd_name=None, duration=0, timeout=0, start_time=timer()):
@@ -255,7 +254,7 @@ def _get_job_status(job_id="*", owner="*", job_name="*", dd_name=None, dd_scan=T
 
             # this section only works on zoau 1.2.3/+ vvv
 
-            if ZOAU_API_VERSION > "1.2.2":
+            if zoau_version_checker.is_zoau_version_higher_than("1.2.2"):
                 job["job_class"] = entry.job_class
                 job["svc_class"] = entry.svc_class
                 job["priority"] = entry.priority
@@ -263,7 +262,7 @@ def _get_job_status(job_id="*", owner="*", job_name="*", dd_name=None, dd_scan=T
                 job["creation_date"] = str(entry.creation_datetime)[0:10]
                 job["creation_time"] = str(entry.creation_datetime)[12:]
                 job["queue_position"] = entry.queue_position
-            if ZOAU_API_VERSION >= "1.2.4":
+            if zoau_version_checker.is_zoau_version_higher_than("1.2.3"):
                 job["program_name"] = entry.program_name
 
             # this section only works on zoau 1.2.3/+ ^^^

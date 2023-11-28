ece9be4795eccbdf9c240eaa2f78e48b3683dda4
Rich Parker <richp405@gmail.com>
2023-11-28 10:21:53 -0500

    added print_results to catalog/uncatalog to see why it failed

--- tests/functional/modules/test_zos_data_set_func.py
+++ tests/functional/modules/test_zos_data_set_func.py
@@ -162,6 +162,9 @@ def test_data_set_catalog_and_uncatalog(ansible_zos_module, jcl):
             src=TEMP_PATH + "/SAMPLE", location="USS", wait=True, wait_time_s=30
         )
         # verify data set creation was successful
+        print("\n=v=v=v=v=v======\n")
+        print_results(results)
+        print("\n=^=^=^=^=^======\n")
         for result in results.contacted.values():
             if(result.get("jobs")[0].get("ret_code") is None):
                 submitted_job_id = result.get("jobs")[0].get("job_id")

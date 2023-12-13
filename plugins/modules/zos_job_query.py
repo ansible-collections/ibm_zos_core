f276065ac665d3e0f5a498a2a7c60e5008596a6e
André Marcel Gutiérrez Benítez <amgutierrezbenitez@hotmail.com>
2023-12-13 14:25:31 -0600

    Remove re

--- plugins/modules/zos_job_query.py
+++ plugins/modules/zos_job_query.py
@@ -263,7 +263,6 @@ from ansible_collections.ibm.ibm_zos_core.plugins.module_utils import (
 )
 from ansible.module_utils.basic import AnsibleModule
 from ansible.module_utils._text import to_text
-import re
 
 
 def run_module():

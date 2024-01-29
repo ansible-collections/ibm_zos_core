d040df6d696f056f3668c63f10c2a36d925bc079
Fernando Flores <fernandofloresdev@gmail.com>
2024-01-29 14:14:38 -0600

    Cherry picked removed hard coded content from staging-v1.9.0-beta.1
    Added preferred volumes changes
    
    [v1.10.0] [zos_copy] Enhance zos_copy performance when copying multiple PDS members  (#1183)
    
    * [v1.9.0] [zos_copy] Enhancement/764/copy members (#1176)
    
    * Changed member copy into a bulk call
    
    * Modified copy to members
    
    * Cleaned code
    
    * Removed hardcoded content from zos_copy tests
    
    * Added fix for uss files
    
    * Added distinction between uss and mvs
    
    * Added alias fix
    
    * Moved the copy section to below
    
    * Modified for seq test
    
    * Added fix for copy dest lock
    
    * Added msgs for debugging
    
    * Added final changes to member copy
    
    * Added copy for when seq to pdse
    
    * Add changelog
    
    * Added a line into docs
    
    * Modified doc
    
    * Modified doc
    
    * Update changelog
    
    * Created a new changelog
    
    * Corrected typo
    
    Fix for empty volumes on test_config
    
    Added comment to remind uncomment test case in the future
    
    Add more validation
    
    Add more validation
    
    Add more validation
    
    Add to config volumes and remove upper case variable name

--- tests/functional/modules/test_zos_fetch_func.py
+++ tests/functional/modules/test_zos_fetch_func.py
@@ -21,6 +21,9 @@ from hashlib import sha256
 from ansible.utils.hashing import checksum
 from shellescape import quote
 
+from ibm_zos_core.tests.helpers.volumes import Volume_Handler
+from ibm_zos_core.tests.helpers.dataset import get_tmp_ds_name
+
 __metaclass__ = type
 
 
@@ -29,12 +32,6 @@ DUMMY DATA == LINE 02 ==
 DUMMY DATA == LINE 03 ==
 """
 
-
-TEST_PS = "USER.PRIV.TEST"
-TEST_PS_VB = "USER.PRIV.PSVB"
-TEST_PDS = "USER.PRIV.TESTPDS"
-TEST_PDS_MEMBER = "USER.PRIV.TESTPDS(MEM1)"
-TEST_VSAM = "FETCH.TEST.VS"
 FROM_ENCODING = "IBM-1047"
 TO_ENCODING = "ISO8859-1"
 USS_FILE = "/tmp/fetch.data"
@@ -48,18 +45,17 @@ KSDS_CREATE_JCL = """//CREKSDS    JOB (T043JM,JM00,1,0,0,0),'CREATE KSDS',CLASS=
 //STEP1  EXEC PGM=IDCAMS
 //SYSPRINT DD  SYSOUT=A
 //SYSIN    DD  *
-    DELETE FETCH.TEST.VS
     SET MAXCC=0
     DEFINE CLUSTER                          -
-    (NAME(FETCH.TEST.VS)                  -
+    (NAME({1})                  -
     INDEXED                                -
     KEYS(4 0)                            -
     RECSZ(200 200)                         -
     RECORDS(100)                           -
     SHAREOPTIONS(2 3)                      -
-    VOLUMES(000000) )                      -
-    DATA (NAME(FETCH.TEST.VS.DATA))       -
-    INDEX (NAME(FETCH.TEST.VS.INDEX))
+    VOLUMES({0}) )                      -
+    DATA (NAME({1}.DATA))       -
+    INDEX (NAME({1}.INDEX))
 /*
 """
 KSDS_REPRO_JCL = """//DOREPRO    JOB (T043JM,JM00,1,0,0,0),'CREATE KSDS',CLASS=R,
@@ -90,21 +86,21 @@ def extract_member_name(data_set):
         member += data_set[i]
     return member
 
-def create_and_populate_test_ps_vb(ansible_zos_module):
+def create_and_populate_test_ps_vb(ansible_zos_module, name):
     params=dict(
-        name=TEST_PS_VB,
+        name=name,
         type='SEQ',
         record_format='VB',
         record_length='3180',
         block_size='3190'
     )
     ansible_zos_module.all.zos_data_set(**params)
-    ansible_zos_module.all.shell(cmd="decho \"{0}\" \"{1}\"".format(TEST_DATA, TEST_PS_VB))
+    ansible_zos_module.all.shell(cmd="decho \"{0}\" \"{1}\"".format(TEST_DATA, name))
 
 
-def delete_test_ps_vb(ansible_zos_module):
+def delete_test_ps_vb(ansible_zos_module, name):
     params=dict(
-        name=TEST_PS_VB,
+        name=name,
         state='absent'
     )
     ansible_zos_module.all.zos_data_set(**params)
@@ -191,6 +187,7 @@ def test_fetch_uss_file_present_on_local_machine(ansible_zos_module):
 
 def test_fetch_sequential_data_set_fixed_block(ansible_zos_module):
     hosts = ansible_zos_module
+    TEST_PS = get_tmp_ds_name()
     hosts.all.zos_data_set(name=TEST_PS, state="present", type="SEQ", size="5m")
     hosts.all.shell(cmd="decho \"{0}\" \"{1}\"".format(TEST_DATA, TEST_PS))
     params = dict(src=TEST_PS, dest="/tmp/", flat=True)
@@ -211,7 +208,8 @@ def test_fetch_sequential_data_set_fixed_block(ansible_zos_module):
 
 def test_fetch_sequential_data_set_variable_block(ansible_zos_module):
     hosts = ansible_zos_module
-    create_and_populate_test_ps_vb(ansible_zos_module)
+    TEST_PS_VB = get_tmp_ds_name(3)
+    create_and_populate_test_ps_vb(ansible_zos_module, TEST_PS_VB)
     params = dict(src=TEST_PS_VB, dest="/tmp/", flat=True)
     dest_path = "/tmp/" + TEST_PS_VB
     try:
@@ -225,12 +223,14 @@ def test_fetch_sequential_data_set_variable_block(ansible_zos_module):
     finally:
         if os.path.exists(dest_path):
             os.remove(dest_path)
-        delete_test_ps_vb(ansible_zos_module)
+        delete_test_ps_vb(ansible_zos_module, TEST_PS_VB)
 
 
 def test_fetch_partitioned_data_set(ansible_zos_module):
     hosts = ansible_zos_module
+    TEST_PDS = get_tmp_ds_name()
     hosts.all.zos_data_set(name=TEST_PDS, state="present", type="PDSE")
+    TEST_PDS_MEMBER = TEST_PDS + "(MEM)"
     hosts.all.zos_data_set(name=TEST_PDS_MEMBER, type="member")
     hosts.all.shell(cmd="decho \"{0}\" \"{1}\"".format(TEST_DATA, TEST_PDS_MEMBER))
     params = dict(src=TEST_PDS, dest="/tmp/", flat=True)
@@ -250,30 +250,33 @@ def test_fetch_partitioned_data_set(ansible_zos_module):
             shutil.rmtree(dest_path)
 
 
-def test_fetch_vsam_data_set(ansible_zos_module):
+def test_fetch_vsam_data_set(ansible_zos_module, volumes_on_systems):
     hosts = ansible_zos_module
-    TEMP_JCL_PATH = "/tmp/ansible"
-    dest_path = "/tmp/" + TEST_VSAM
+    temp_jcl_path = "/tmp/ansible"
+    test_vsam = get_tmp_ds_name()
+    dest_path = "/tmp/" + test_vsam
+    volumes = Volume_Handler(volumes_on_systems)
+    volume_1 = volumes.get_available_vol()
     try:
         # start by creating the vsam dataset (could use a helper instead? )
-        hosts.all.file(path=TEMP_JCL_PATH, state="directory")
+        hosts.all.file(path=temp_jcl_path, state="directory")
         hosts.all.shell(
-            cmd="echo {0} > {1}/SAMPLE".format(quote(KSDS_CREATE_JCL), TEMP_JCL_PATH)
+            cmd="echo {0} > {1}/SAMPLE".format(quote(KSDS_CREATE_JCL.format(volume_1, test_vsam)), temp_jcl_path)
         )
         hosts.all.zos_job_submit(
-            src="{0}/SAMPLE".format(TEMP_JCL_PATH), location="USS", wait=True
+            src="{0}/SAMPLE".format(temp_jcl_path), location="USS", wait=True
         )
         hosts.all.shell(cmd="echo \"{0}\c\" > {1}".format(TEST_DATA, USS_FILE))
         hosts.all.zos_encode(
             src=USS_FILE,
-            dest=TEST_VSAM,
+            dest=test_vsam,
             encoding={
                 "from": FROM_ENCODING,
                 "to": TO_ENCODING,
             },
         )
 
-        params = dict(src=TEST_VSAM, dest="/tmp/", flat=True, is_binary=True)
+        params = dict(src=test_vsam, dest="/tmp/", flat=True, is_binary=True)
         results = hosts.all.zos_fetch(**params)
         for result in results.contacted.values():
             assert result.get("changed") is True
@@ -291,7 +294,7 @@ def test_fetch_vsam_data_set(ansible_zos_module):
             None
             os.remove(dest_path)
         hosts.all.file(path=USS_FILE, state="absent")
-        hosts.all.file(path=TEMP_JCL_PATH, state="absent")
+        hosts.all.file(path=temp_jcl_path, state="absent")
 
 
 def test_fetch_vsam_empty_data_set(ansible_zos_module):
@@ -316,7 +319,9 @@ def test_fetch_vsam_empty_data_set(ansible_zos_module):
 
 def test_fetch_partitioned_data_set_member_in_binary_mode(ansible_zos_module):
     hosts = ansible_zos_module
+    TEST_PDS = get_tmp_ds_name()
     hosts.all.zos_data_set(name=TEST_PDS, state="present")
+    TEST_PDS_MEMBER = TEST_PDS + "(MEM)"
     hosts.all.zos_data_set(name=TEST_PDS_MEMBER, type="member")
     hosts.all.shell(cmd="decho \"{0}\" \"{1}\"".format(TEST_DATA, TEST_PDS_MEMBER))
     params = dict(
@@ -341,6 +346,7 @@ def test_fetch_partitioned_data_set_member_in_binary_mode(ansible_zos_module):
 
 def test_fetch_sequential_data_set_in_binary_mode(ansible_zos_module):
     hosts = ansible_zos_module
+    TEST_PS = get_tmp_ds_name()
     hosts.all.zos_data_set(name=TEST_PS, state="present", type="SEQ", size="5m")
     hosts.all.shell(cmd="decho \"{0}\" \"{1}\"".format(TEST_DATA, TEST_PS))
     params = dict(src=TEST_PS, dest="/tmp/", flat=True, is_binary=True)
@@ -361,7 +367,9 @@ def test_fetch_sequential_data_set_in_binary_mode(ansible_zos_module):
 
 def test_fetch_partitioned_data_set_binary_mode(ansible_zos_module):
     hosts = ansible_zos_module
+    TEST_PDS = get_tmp_ds_name()
     hosts.all.zos_data_set(name=TEST_PDS, state="present", type="PDSE")
+    TEST_PDS_MEMBER = TEST_PDS + "(MEM)"
     hosts.all.zos_data_set(name=TEST_PDS_MEMBER, type="member")
     hosts.all.shell(cmd="decho \"{0}\" \"{1}\"".format(TEST_DATA, TEST_PDS_MEMBER))
     params = dict(src=TEST_PDS, dest="/tmp/", flat=True, is_binary=True)
@@ -383,7 +391,7 @@ def test_fetch_partitioned_data_set_binary_mode(ansible_zos_module):
 
 def test_fetch_sequential_data_set_empty(ansible_zos_module):
     hosts = ansible_zos_module
-    src = "USER.TEST.EMPTY.SEQ"
+    src = get_tmp_ds_name()
     params = dict(src=src, dest="/tmp/", flat=True)
     dest_path = "/tmp/" + src
     try:
@@ -404,7 +412,7 @@ def test_fetch_sequential_data_set_empty(ansible_zos_module):
 
 def test_fetch_partitioned_data_set_empty_fails(ansible_zos_module):
     hosts = ansible_zos_module
-    pds_name = "ZOS.FETCH.TEST.PDS"
+    pds_name = get_tmp_ds_name()
     hosts.all.zos_data_set(
         name=pds_name,
         type="pds",
@@ -425,7 +433,7 @@ def test_fetch_partitioned_data_set_empty_fails(ansible_zos_module):
 
 def test_fetch_partitioned_data_set_member_empty(ansible_zos_module):
     hosts = ansible_zos_module
-    pds_name = "ZOS.FETCH.TEST.PDS"
+    pds_name = get_tmp_ds_name()
     hosts.all.zos_data_set(
         name=pds_name,
         type="pds",
@@ -434,8 +442,9 @@ def test_fetch_partitioned_data_set_member_empty(ansible_zos_module):
         record_format="fba",
         record_length=25,
     )
+    hosts.all.zos_data_set(name=pds_name, type="pds")
     hosts.all.zos_data_set(name=pds_name + "(MYDATA)", type="MEMBER", replace="yes")
-    params = dict(src="ZOS.FETCH.TEST.PDS(MYDATA)", dest="/tmp/", flat=True)
+    params = dict(src=pds_name + "(MYDATA)", dest="/tmp/", flat=True)
     dest_path = "/tmp/MYDATA"
     try:
         results = hosts.all.zos_fetch(**params)
@@ -482,8 +491,9 @@ def test_fetch_missing_uss_file_fails(ansible_zos_module):
 
 def test_fetch_missing_mvs_data_set_does_not_fail(ansible_zos_module):
     hosts = ansible_zos_module
+    src = get_tmp_ds_name()
     params = dict(
-        src="FETCH.TEST.DATA.SET", dest="/tmp/", flat=True, fail_on_missing=False
+        src=src, dest="/tmp/", flat=True, fail_on_missing=False
     )
     try:
         results = hosts.all.zos_fetch(**params)
@@ -498,6 +508,7 @@ def test_fetch_missing_mvs_data_set_does_not_fail(ansible_zos_module):
 
 def test_fetch_partitioned_data_set_member_missing_fails(ansible_zos_module):
     hosts = ansible_zos_module
+    TEST_PDS = get_tmp_ds_name()
     params = dict(src=TEST_PDS + "(DUMMY)", dest="/tmp/", flat=True)
     try:
         results = hosts.all.zos_fetch(**params)
@@ -510,7 +521,8 @@ def test_fetch_partitioned_data_set_member_missing_fails(ansible_zos_module):
 
 def test_fetch_mvs_data_set_missing_fails(ansible_zos_module):
     hosts = ansible_zos_module
-    params = dict(src="ZOS.FETCH.TEST.PDS", dest="/tmp/", flat=True)
+    src = get_tmp_ds_name()
+    params = dict(src=src, dest="/tmp/", flat=True)
     try:
         results = hosts.all.zos_fetch(**params)
         for result in results.contacted.values():
@@ -522,6 +534,7 @@ def test_fetch_mvs_data_set_missing_fails(ansible_zos_module):
 
 def test_fetch_sequential_data_set_replace_on_local_machine(ansible_zos_module):
     hosts = ansible_zos_module
+    TEST_PS = get_tmp_ds_name()
     hosts.all.zos_data_set(name=TEST_PS, state="present", type="SEQ", size="5m")
     ds_name = TEST_PS
     hosts.all.zos_data_set(name=TEST_PS, state="present")
@@ -546,7 +559,7 @@ def test_fetch_sequential_data_set_replace_on_local_machine(ansible_zos_module):
 
 def test_fetch_partitioned_data_set_replace_on_local_machine(ansible_zos_module):
     hosts = ansible_zos_module
-    pds_name = "ZOS.FETCH.TEST.PDS"
+    pds_name = get_tmp_ds_name()
     dest_path = "/tmp/" + pds_name
     full_path = dest_path + "/MYDATA"
     hosts.all.zos_data_set(
@@ -596,6 +609,7 @@ def test_fetch_uss_file_insufficient_write_permission_fails(ansible_zos_module):
 
 def test_fetch_pds_dir_insufficient_write_permission_fails(ansible_zos_module):
     hosts = ansible_zos_module
+    TEST_PDS = get_tmp_ds_name()
     dest_path = "/tmp/" + TEST_PDS
     os.mkdir(dest_path)
     os.chmod(dest_path, stat.S_IREAD)
@@ -611,12 +625,14 @@ def test_fetch_pds_dir_insufficient_write_permission_fails(ansible_zos_module):
 
 def test_fetch_use_data_set_qualifier(ansible_zos_module):
     hosts = ansible_zos_module
-    dest_path = "/tmp/TEST.USER.QUAL"
-    hosts.all.zos_data_set(name="OMVSADM.TEST.USER.QUAL", type="seq", state="present")
-    params = dict(src="TEST.USER.QUAL", dest="/tmp/", flat=True, use_qualifier=True)
+    src = get_tmp_ds_name()[:25]
+    dest_path = "/tmp/"+ src
+    hosts.all.zos_data_set(name="OMVSADM." + src, type="seq", state="present")
+    params = dict(src=src, dest="/tmp/", flat=True, use_qualifier=True)
     try:
         results = hosts.all.zos_fetch(**params)
         for result in results.contacted.values():
+            print(result)
             assert result.get("changed") is True
             assert result.get("data_set_type") == "Sequential"
             assert result.get("module_stderr") is None
@@ -624,7 +640,7 @@ def test_fetch_use_data_set_qualifier(ansible_zos_module):
     finally:
         if os.path.exists(dest_path):
             os.remove(dest_path)
-        hosts.all.zos_data_set(src="OMVSADM.TEST.USER.QUAL", state="absent")
+        hosts.all.zos_data_set(src="OMVSADM." + src, state="absent")
 
 
 def test_fetch_flat_create_dirs(ansible_zos_module, z_python_interpreter):

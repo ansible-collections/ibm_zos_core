5e77eec423d8041ec4705a5ef0bfe5d114417cc9
Rich Parker <richp405@gmail.com>
2024-01-29 12:59:21 -0500

    added delay for uncatalog to kick in

--- tests/functional/modules/test_zos_data_set_func.py
+++ tests/functional/modules/test_zos_data_set_func.py
@@ -154,9 +154,9 @@ def test_data_set_catalog_and_uncatalog(ansible_zos_module, jcl, volumes_on_syst
     volume_1 = "000000"
     dataset = get_tmp_ds_name(2, 2)
     try:
-        # hosts.all.zos_data_set(
-        #     name=dataset, state="cataloged", volumes=volume_1
-        # )
+        hosts.all.zos_data_set(
+            name=dataset, state="cataloged", volumes=volume_1
+        )
         hosts.all.zos_data_set(name=dataset, state="absent")
         print( "\nVVVVV -- initial\n")
         print( "vols: {0} \nv1: {1} \nds: {2}\n".format(volumes, volume_1, dataset))
@@ -331,10 +331,15 @@ def test_data_set_absent_when_uncataloged(ansible_zos_module, jcl, volumes_on_sy
         results = hosts.all.zos_data_set(name=dataset, state="uncataloged")
         for result in results.contacted.values():
             assert result.get("changed") is True
+
+        time.sleep(5)
+
         # ensure data set absent
         results = hosts.all.zos_data_set(
             name=dataset, state="absent", volumes=volume_1
         )
+        print( "\nVVV ---- abs when uncat\n")
+        print_results( results )
         for result in results.contacted.values():
             assert result.get("changed") is True
     finally:

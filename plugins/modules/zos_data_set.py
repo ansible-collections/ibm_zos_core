f2f05bdef75fdac6dc2d9f04abf65e51d66e92b6
Rich Parker <richp405@gmail.com>
2024-02-05 16:18:59 -0500

    added batch option into capitalizer loop

--- plugins/modules/zos_data_set.py
+++ plugins/modules/zos_data_set.py
@@ -1419,10 +1419,15 @@ def run_module():
             module.params["record_format"] = None
 
     # force parameters to upper case
-    select_list = ( "type", "space_type", "record_format")
-    if module.params.get["batch"] is None:
+    select_list = ("type", "space_type", "record_format")
+    if module.parame.get["batch"] is not None:
+        for entry in module.params.get("batch"):
+          for item in select_list:
+            if entry.params.get(item) is not None:
+                entry.params[item] = module.params.get(item).upper()
+    else:
         for item in select_list:
-            if module.params.get( item ) is not None:
+            if module.params.get(item) is not None:
                 module.params[item] = module.params.get(item).upper()
 
     if not module.check_mode:

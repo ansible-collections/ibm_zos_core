ae70caaf46f14d149990461e0cc1ca4e0ee43c6c
ddimatos <dimatos@gmail.com>
2024-03-10 17:37:39 -0700

    Update doc for zos_archive to reference src over path
    Signed-off-by: ddimatos <dimatos@gmail.com>

--- plugins/modules/zos_job_submit.py
+++ plugins/modules/zos_job_submit.py
@@ -126,6 +126,13 @@ extends_documentation_fragment:
 notes:
   - For supported character sets used to encode data, refer to the
     L(documentation,https://ibm.github.io/z_ansible_collections_doc/ibm_zos_core/docs/source/resources/character_set.html).
+  - This module uses L(zos_copy,./zos_copy.html) to copy local scripts to
+    the remote machine which uses SFTP (Secure File Transfer Protocol) for the
+    underlying transfer protocol; SCP (secure copy protocol) and Co:Z SFTP are not
+    supported. In the case of Co:z SFTP, you can exempt the Ansible user id on z/OS
+    from using Co:Z thus falling back to using standard SFTP. If the module detects
+    SCP, it will temporarily use SFTP for transfers, if not available, the module
+    will fail.
 """
 
 RETURN = r"""

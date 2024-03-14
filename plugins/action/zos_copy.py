8c046e432d6bc2d509386a811b37952eb26d52ec
Fernando Flores <fernandofloresdev@gmail.com>
2024-03-13 20:08:22 -0600

    Placed temporary file into tmp folder

--- plugins/action/zos_copy.py
+++ plugins/action/zos_copy.py
@@ -304,7 +304,8 @@ class ActionModule(ActionBase):
     def _copy_to_remote(self, src, is_dir=False, ignore_stderr=False):
         """Copy a file or directory to the remote z/OS system """
 
-        temp_path = "/{0}/{1}".format(gettempprefix(), os.path.basename(src))
+        temp_path = "/{0}/{1}/{2}".format(gettempprefix(), _create_temp_path_name(), os.path.basename(src))
+        self._connection.exec_command("mkdir -p {0}".format(os.path.dirname(temp_path)))
         _src = src.replace("#", "\\#")
         _sftp_action = 'put'
 

103d75bff7084f17f0e712d180f67fd6c3fee418
ddimatos <dimatos@gmail.com>
2024-03-16 23:26:49 -0700

    Update submit modules doc
    Signed-off-by: ddimatos <dimatos@gmail.com>

--- plugins/modules/zos_job_submit.py
+++ plugins/modules/zos_job_submit.py
@@ -25,9 +25,8 @@ author:
     - "Demetrios Dimatos (@ddimatos)"
 short_description: Submit JCL
 description:
-    - Submit JCL from a data set, USS, or from the controller.
-    - Submit a job and optionally monitor for completion.
-    - Optionally, wait a designated time until the job finishes.
+    - Submit JCL in a data set, USS file, or file on the controller.
+    - Submit a job and monitor for completion.
     - For an uncataloged dataset, specify the volume serial number.
 version_added: "1.0.0"
 options:
@@ -59,11 +58,10 @@ options:
     default: false
     type: bool
     description:
-      - Setting this option will yield no change, it is deprecated. There is no
+      - Setting this option will yield no change, it is disabled. There is no
         no need to set I(wait); setting I(wait_times_s) is the correct way to
         configure the amount of tme to wait for a job to execute.
-      - Configuring wait used by the L(zos_job_submit,./zos_job_submit.html) module has been
-        deprecated and will be removed in ibm.ibm_zos_core collection.
+      - This option will be removed in ibm.ibm_zos_core collection version 1.10.0
       - See option I(wait_time_s).
   wait_time_s:
     required: false
@@ -235,28 +233,36 @@ jobs:
       contains:
         msg:
           description:
-            Return code resulting from the job submission. Jobs that take
-            longer to assign a value can have a value of '?'.
+            - Job status resulting from the job submission.
+            - Job status `ABEND` indicates the job ended abnormally.
+            - Job status `AC` indicates the job is active, often a started task or job taking long.
+            - Job status `CAB` indicates a converter abend.
+            - Job status `CANCELED` indicates the job was canceled.
+            - Job status `CNV` indicates a converter error.
+            - Job status `FLU` indicates the job was flushed.
+            - Job status `JCLERR` or `JCL ERROR` indicates the JCL has an error.
+            - Job status `SEC` or `SEC ERROR` indicates the job as encountered a security error.
+            - Job status `SYS` indicates a system failure.
+            - Job status `?` indicates status can not be determined.
           type: str
-          sample: CC 0000
+          sample: AC
         msg_code:
           description:
-            Return code extracted from the `msg` so that it can be evaluated
-            as a string. Jobs that take longer to assign a value can have a
-            value of '?'.
+            - The return code from the submitted job as a string.
           type: str
           sample: 0000
         msg_txt:
           description:
-             Returns additional information related to the job. Jobs that take
-             longer to assign a value can have a value of '?'.
+             Returns additional information related to the submitted job.
           type: str
-          sample: The job completion code (CC) was not available in the job
-                  output, please review the job log."
+          sample: The job JOB00551 was run with special job processing TYPRUN=SCAN.
+                  This will result in no completion, return code or job steps and
+                  changed will be false.
         code:
           description:
-             Return code converted to an integer value (when possible).
-             For JCL ERRORs, this will be None.
+            - The return code converted to an integer value when available.
+            - Jobs which have no return code will return NULL, such is the case
+              of a job that errors or is active.
           type: int
           sample: 0
         steps:

052a82ae559a268c514a68629c0daa1813880948
Fernando Flores <fernandofloresdev@gmail.com>
2024-04-15 15:12:38 -0600

    Added fix

--- tests/functional/modules/test_zos_find_func.py
+++ tests/functional/modules/test_zos_find_func.py
@@ -14,6 +14,7 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
+import pytest
 from ibm_zos_core.tests.helpers.volumes import Volume_Handler
 
 SEQ_NAMES = [
@@ -32,6 +33,7 @@ VSAM_NAMES = [
     "TEST.FIND.VSAM.FUNCTEST.FIRST"
 ]
 
+DATASET_TYPES = ['seq', 'pds', 'pdse']
 
 def create_vsam_ksds(ds_name, ansible_zos_module, volume="000000"):
     hosts = ansible_zos_module
@@ -216,13 +218,14 @@ def test_find_data_sets_older_than_age(ansible_zos_module):
         assert val.get('matched') == 2
 
 
-def test_find_data_sets_larger_than_size(ansible_zos_module):
+@pytest.mark.parametrize("ds_type", DATASET_TYPES)
+def test_find_data_sets_larger_than_size(ansible_zos_module, ds_type):
     hosts = ansible_zos_module
     TEST_PS1 = 'TEST.PS.ONE'
     TEST_PS2 = 'TEST.PS.TWO'
     try:
-        res = hosts.all.zos_data_set(name=TEST_PS1, state="present", size="5m")
-        res = hosts.all.zos_data_set(name=TEST_PS2, state="present", size="5m")
+        res = hosts.all.zos_data_set(name=TEST_PS1, state="present", size="5m", type=ds_type)
+        res = hosts.all.zos_data_set(name=TEST_PS2, state="present", size="5m", type=ds_type)
         find_res = hosts.all.zos_find(patterns=['TEST.PS.*'], size="1k")
         for val in find_res.contacted.values():
             assert len(val.get('data_sets')) == 2

75aef862f9ec5bedef75a9c872a18bab31864f96
IsaacVRey <isaac.vega.rey@gmail.com>
2024-04-24 13:29:06 -0600

    Update test suites on functional/modules/test_zos_tso_command_func.py to be pylint correct

--- tests/functional/modules/test_zos_tso_command_func.py
+++ tests/functional/modules/test_zos_tso_command_func.py
@@ -1,3 +1,8 @@
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=redefined-outer-name
+# pylint: disable=too-many-lines
 # -*- coding: utf-8 -*-
 
 # Copyright (c) IBM Corporation 2019, 2024
@@ -15,9 +20,6 @@ from __future__ import absolute_import, division, print_function
 
 __metaclass__ = type
 
-import ansible.constants
-import ansible.errors
-import ansible.utils
 from ibm_zos_core.tests.helpers.dataset import get_tmp_ds_name
 
 
@@ -51,7 +53,8 @@ def test_zos_tso_command_allocate_listing_delete(ansible_zos_module):
     hosts = ansible_zos_module
     default_temp_dataset = get_tmp_ds_name()
     command_string = [
-        "alloc da('{0}') catalog lrecl(133) blksize(13300) recfm(f b) dsorg(po) cylinders space(5,5) dir(5)".format(default_temp_dataset)
+        f"alloc da('{default_temp_dataset}') "+
+        "catalog lrecl(133) blksize(13300) recfm(f b) dsorg(po) cylinders space(5,5) dir(5)"
     ]
     results_allocate = hosts.all.zos_tso_command(commands=command_string)
     # Validate the correct allocation of dataset
@@ -60,34 +63,35 @@ def test_zos_tso_command_allocate_listing_delete(ansible_zos_module):
             assert item.get("rc") == 0
         assert result.get("changed") is True
     # Validate listds of datasets and validate LISTDS using alias param 'command' of auth command
-    results = hosts.all.zos_tso_command(commands=["LISTDS '{0}'".format(default_temp_dataset)])
+    results = hosts.all.zos_tso_command(commands=[f"LISTDS '{default_temp_dataset}'"])
     for result in results.contacted.values():
         for item in result.get("output"):
             assert item.get("rc") == 0
         assert result.get("changed") is True
     # Validate LISTDS using alias param 'command'
-    results = hosts.all.zos_tso_command(command="LISTDS '{0}'".format(default_temp_dataset))
+    results = hosts.all.zos_tso_command(command=f"LISTDS '{default_temp_dataset}'")
     for result in results.contacted.values():
         for item in result.get("output"):
             assert item.get("rc") == 0
         assert result.get("changed") is True
     # Validate LISTCAT command and an unauth command
     results = hosts.all.zos_tso_command(
-        commands=["LISTCAT ENT('{0}')".format(default_temp_dataset)]
+        commands=[f"LISTCAT ENT('{default_temp_dataset}')"]
     )
     for result in results.contacted.values():
         for item in result.get("output"):
             assert item.get("rc") == 0
         assert result.get("changed") is True
     # Validate remove dataset
-    results = hosts.all.zos_tso_command(commands=["delete '{0}'".format(default_temp_dataset)])
+    results = hosts.all.zos_tso_command(commands=[f"delete '{default_temp_dataset}'"])
     for result in results.contacted.values():
         for item in result.get("output"):
             assert item.get("rc") == 0
         assert result.get("changed") is True
-    # Expect the tso_command to fail here because the previous command will have already deleted the data set
+    # Expect the tso_command to fail here because
+    # the previous command will have already deleted the data set
     # Validate data set was removed by previous call
-    results = hosts.all.zos_tso_command(commands=["delete '{0}'".format(default_temp_dataset)])
+    results = hosts.all.zos_tso_command(commands=[f"delete '{default_temp_dataset}'"])
     for result in results.contacted.values():
         for item in result.get("output"):
             assert item.get("rc") == 8
@@ -135,20 +139,11 @@ def test_zos_tso_command_multiple_commands(ansible_zos_module):
 # The command that kicks off rc>0 which is allowed
 def test_zos_tso_command_maxrc(ansible_zos_module):
     hosts = ansible_zos_module
-    results = hosts.all.zos_tso_command(commands=["LISTDSD DATASET('HLQ.DATA.SET') ALL GENERIC"],max_rc=4)
+    results = hosts.all.zos_tso_command(
+        commands=["LISTDSD DATASET('HLQ.DATA.SET') ALL GENERIC"],
+        max_rc=4
+    )
     for result in results.contacted.values():
         for item in result.get("output"):
-            assert item.get("rc") < 5
-        assert result.get("changed") is True
-
-
-# The positive test
-# The command that kicks off rc>0 which is allowed
-def test_zos_tso_command_maxrc(ansible_zos_module):
-    hosts = ansible_zos_module
-    results = hosts.all.zos_tso_command(commands=["LISTDSD DATASET('HLQ.DATA.SET') ALL GENERIC"],max_rc=4)
-    for result in results.contacted.values():
-        for item in result.get("output"):
-            print( item )
             assert item.get("rc") < 5
         assert result.get("changed") is True

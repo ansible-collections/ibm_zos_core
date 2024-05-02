2d421c464d98f38a0fc1759bea3cd5b486b505c8
Rich Parker <richp405@gmail.com>
2024-05-02 10:59:20 -0400

    corrected changelog fragment to include PR number in link added import to zos_apf to get staticmethod loaded.

--- plugins/modules/zos_apf.py
+++ plugins/modules/zos_apf.py
@@ -296,6 +296,7 @@ from ansible.module_utils._text import to_text
 from ansible.module_utils.basic import AnsibleModule
 from ansible_collections.ibm.ibm_zos_core.plugins.module_utils import (
     better_arg_parser, data_set, backup as Backup)
+from ansible_collections.ibm.ibm_zos_core.plugins.module_utils.data_set import DataSet
 from ansible_collections.ibm.ibm_zos_core.plugins.module_utils.import_handler import (
     ZOAUImportError,
 )

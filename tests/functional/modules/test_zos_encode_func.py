f6d924b7a9690b7c579b8e5ce5cfd58a89cd75ac
André Marcel Gutiérrez Benítez <68956970+AndreMarcel99@users.noreply.github.com>
2024-06-03 15:00:57 -0600

    Update test_zos_encode_func.py

--- tests/functional/modules/test_zos_encode_func.py
+++ tests/functional/modules/test_zos_encode_func.py
@@ -1,8 +1,3 @@
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
-# pylint: disable=redefined-outer-name
-# pylint: disable=too-many-lines
 # -*- coding: utf-8 -*-
 
 # Copyright (c) IBM Corporation 2019, 2024

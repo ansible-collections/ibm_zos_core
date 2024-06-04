6fcb8f6ba21a62c18012c15fc40603e7f7a2afb4
André Marcel Gutiérrez Benítez <68956970+AndreMarcel99@users.noreply.github.com>
2024-06-04 12:16:08 -0600

    Update test_zos_fetch_func.py

--- tests/functional/modules/test_zos_fetch_func.py
+++ tests/functional/modules/test_zos_fetch_func.py
@@ -1,9 +1,3 @@
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
-# pylint: disable=redefined-outer-name
-# pylint: disable=too-many-lines
-# pylint: disable=invalid-name
 # -*- coding: utf-8 -*-
 
 # Copyright (c) IBM Corporation 2020, 2024

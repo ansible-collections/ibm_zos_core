0ac81703227ec17d6a4931da7c6be23d64b53a74
Fernando Flores <fernandofloresdev@gmail.com>
2024-08-12 15:17:34 -0600

    Fixed trailing parenthesis

--- plugins/modules/zos_blockinfile.py
+++ plugins/modules/zos_blockinfile.py
@@ -39,7 +39,7 @@ options:
         PS (sequential data set), member of a PDS or PDSE, PDS, PDSE.
       - The USS file must be an absolute pathname.
       - Generation data set (GDS) relative name of generation already
-        created.  C(e.g. SOME.CREATION(-1).)
+        created.  C(e.g. SOME.CREATION(-1\).)
     type: str
     aliases: [ path, destfile, name ]
     required: true

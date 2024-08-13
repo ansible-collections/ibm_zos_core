bf0467993b753de27cca84d19d7f81b1821354de
ddimatos <dimatos@gmail.com>
2024-08-13 11:22:37 -0700

    Update for RHEL and Ubuntu
    Signed-off-by: ddimatos <dimatos@gmail.com>

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -354,19 +354,24 @@ create_venv_and_pip_install_req(){
 
                 # On Ubuntu ensurepip is not available in LTS 24.x, support is limited
                 if echo "$DISTRO" |grep 'Ubuntu' >/dev/null; then
-                    # This approach does not get the *-venv installed, taking second approach
-                    # sudo add-apt-repository universe -y
-                    # sudo apt update
-                    # sudo apt install python3-pip
-                    # apt install python$VERSION_PYTHON-venv -y
+                    # This is a less repository driven approach to setting up the venv, for safe keeping, commenting it out.
+                    # ${VERSION_PYTHON_PATH} -m venv --without-pip "${VENV_HOME_MANAGED}"/"${venv_name}"
+                    # curl https://bootstrap.pypa.io/get-pip.py -o ${VENV_HOME_MANAGED}/${venv_name}/bin/get-pip.py
+                    # ${VENV_HOME_MANAGED}/${venv_name}/bin/python${VERSION_PYTHON} ${VENV_HOME_MANAGED}/${venv_name}/bin/get-pip.py
 
-                    ${VERSION_PYTHON_PATH} -m venv --without-pip "${VENV_HOME_MANAGED}"/"${venv_name}"
-                    curl https://bootstrap.pypa.io/get-pip.py -o ${VENV_HOME_MANAGED}/${venv_name}/bin/get-pip.py
-                    ${VENV_HOME_MANAGED}/${venv_name}/bin/python${VERSION_PYTHON} ${VENV_HOME_MANAGED}/${venv_name}/bin/get-pip.py
+                    # 'sshpass' will not be on the host, some additional work to add it.
+                    codename=`cat /etc/os-release | grep UBUNTU_CODENAME | cut -d = -f 2`
+                    add-apt-repository "deb http://us.archive.ubuntu.com/ubuntu/ $codename universe multiverse" -y
+                    apt-get update -y
+                    apt install python3-pip -y
+                    apt install python$VERSION_PYTHON-venv -y
+                    apt install sshpass -y
+                    ${VERSION_PYTHON_PATH} -m venv "${VENV_HOME_MANAGED}"/"${venv_name}"/
                 elif echo "$DISTRO" |grep 'Red Hat Enterprise Linux' >/dev/null; then
                     # Install the Python versions
                     dnf install python${VERSION_PYTHON} -y
                     dnf install python${VERSION_PYTHON}-pip -y
+                    dnf install sshpass
                     ${VERSION_PYTHON_PATH} -m venv "${VENV_HOME_MANAGED}"/"${venv_name}"/
                 fi
             elif echo "$OSTYPE" |grep 'darwin' >/dev/null; then

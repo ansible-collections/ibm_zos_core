2a86f8fc06249e8c7005720c6bfe2bf0e45cb37c
Fernando Flores <fernandofloresdev@gmail.com>
2024-09-17 16:59:03 -0600

    Enabled dd_scan on zos_job_query to return system and subsystem values and updated docs to better align with return object

--- plugins/modules/zos_job_query.py
+++ plugins/modules/zos_job_query.py
@@ -119,6 +119,16 @@ jobs:
          Unique job identifier assigned to the job by JES.
       type: str
       sample: JOB01427
+    system:
+      description:
+         The job entry system that MVS uses to do work.
+      type: str
+      sample: STL1
+    subsystem:
+      description:
+         The job entry subsystem that MVS uses to do work.
+      type: str
+      sample: STL1
     ret_code:
       description:
          Return code output collected from job log.
@@ -129,17 +139,6 @@ jobs:
             Return code or abend resulting from the job submission.
           type: str
           sample: CC 0000
-        msg_code:
-          description:
-            Return code extracted from the `msg` so that it can be evaluated.
-            For example, ABEND(S0C4) would yield "S0C4".
-          type: str
-          sample: S0C4
-        msg_txt:
-          description:
-             Returns additional information related to the job.
-          type: str
-          sample: "No job can be located with this job name: HELLO"
         code:
           description:
              Return code converted to integer value (when possible).

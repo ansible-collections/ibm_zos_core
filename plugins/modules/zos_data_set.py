d1ac0d6e02ebda9973c7598c0384b9aacd4eecee
Ivan Moreno <iamorenosoto@gmail.com>
2024-10-02 09:51:47 -0600

    Fix function calls in zos_data_set

--- plugins/modules/zos_data_set.py
+++ plugins/modules/zos_data_set.py
@@ -1404,14 +1404,16 @@ def perform_data_set_operations(data_set, state, replace, tmp_hlq, force):
         If changes were made.
     """
     changed = False
-    if state == "present" and data_set.data_set_type in ["member", "gdg"]:
-        changed = data_set.ensure_present(replace=replace, tmp_hlq=tmp_hlq)
+    if state == "present" and data_set.data_set_type == "member":
+        changed = data_set.ensure_present(replace=replace, tmphlq=tmp_hlq)
+    elif state == "present" and data_set.data_set_type == "gdg":
+        changed = data_set.ensure_present(replace=replace)
     elif state == "present":
         changed = data_set.ensure_present(replace=replace, tmp_hlq=tmp_hlq, force=force)
     elif state == "absent" and data_set.data_set_type == "member":
-        changed = data_set.ensure_absent(force=force, tmp_hlq=tmp_hlq)
+        changed = data_set.ensure_absent(force=force)
     elif state == "absent" and data_set.data_set_type == "gdg":
-        changed = data_set.ensure_absent(force=force, tmp_hlq=tmp_hlq)
+        changed = data_set.ensure_absent(force=force)
     elif state == "absent":
         changed = data_set.ensure_absent(tmp_hlq=tmp_hlq)
     elif state == "cataloged":

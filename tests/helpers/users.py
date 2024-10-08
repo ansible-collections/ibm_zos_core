504d77613d1c90213b70aaed5852d2f778de4733
ddimatos <dimatos@gmail.com>
2024-10-08 01:58:43 -0700

    Added support for other user types
    Signed-off-by: ddimatos <dimatos@gmail.com>

--- tests/helpers/users.py
+++ tests/helpers/users.py
@@ -12,9 +12,9 @@
 # limitations under the License.
 
 """
-The helpers/users.py file contains various utility functions that
-will support test case user needs. Such cases could be randomly
-generated users with specific limitations or users with UID 0.
+The users.py file contains various utility functions that
+will support functional test cases. For example, request a randomly
+generated user with specific limitations.
 """
 from collections import OrderedDict
 from io import StringIO
@@ -26,41 +26,14 @@ import sys
 import os
 import glob
 
-def get_random_passwd():
-    letters = string.ascii_uppercase
-    start = ''.join(random.choice(letters) for i in range(3))
-    middle = ''.join(str(random.randint(1,9)))
-    finish = ''.join(random.choice(letters) for i in range(4))
-    return f"{start}{middle}{finish}"
-
-def get_random_user():
-    letters = string.ascii_uppercase
-    suffix = ''.join(random.choice(letters) for i in range(4))
-    return "ANSI" + suffix
-
-
 class ManagedUsers (Enum):
     """
     Represents the z/OS users available for functional testing.
 
     Attributes:
     ZOAU_LIMITED_ACCESS_OPERCMD: ManagedUsers
-        A z/OS managed user with restricted access to ZOAU
-        SAF Profile 'MVS.MCSOPER.ZOAU' and SAF Class OPERCMDS
-        with universal access authority set to UACC(NONE). With
-        UACC as NONE, this user will be refused access to the
-        ZOAU opercmd utility.
     ZOS_LIMITED_HLQ: ManagedUsers
-        A z/OS managed user with restricted access to High Level
-        Qualifiers (HLQ): (RESTRICT, NOPERMIT, ....).
     ZOS_LIMITED_TMP_HLQ: ManagedUsers
-        A z/OS managed user with restricted access to temporary
-        High Level Qualifiers (HLQ): (TSTRICT, TNOPERM, ....).
-
-    Methods:
-        user_access() - Returns the user access requested.
-        user_name() - Returns the user name to be used in the functional test.
-        is_equal(user_name) - Checks if this user name is equal to user name.
     """
 
     ZOAU_LIMITED_ACCESS_OPERCMD=("zoau_limited_access_opercmd")
@@ -72,38 +45,79 @@ class ManagedUsers (Enum):
     UACC as NONE, this user will be refused access to the
     ZOAU opercmd utility.
     """
-    ZOS_LIMITED_HLQ=("zos_limited_hlq","ANSUSR02")
+    # TODO: Implement this, use the recommended HLQ
+    ZOS_LIMITED_HLQ=("zos_limited_hlq")
     """
     A z/OS managed user with restricted access to High Level
     Qualifiers (HLQ): (RESTRICT, NOPERMIT, ....).
     """
 
-    ZOS_LIMITED_TMP_HLQ=("zos_limited_tmp_hlq", "ANSUSR03")
+    # TODO: Implement this, use the recommended tmp HLQ
+    ZOS_LIMITED_TMP_HLQ=("zos_limited_tmp_hlq")
     """
     A z/OS managed user with restricted access to temporary
     High Level Qualifiers (HLQ): (TSTRICT, TNOPERM, ....).
     """
 
+    ZOS_BEGIN_WITH_AT_SIGN=("zos_begin_with_at_sign")
+    """
+    A z/OS managed user....
+    """
+
+    ZOS_BEGIN_WITH_POUND=("zos_begin_with_pound")
+    """
+    A z/OS managed user....
+    """
+
+    ZOS_RANDOM_SYMBOLS=("zos_random_symbols")
+    """
+    A z/OS managed user....user ID cannot exceed seven characters and must begin with an alphabetic, # (X'7B'), $ (X'5B'), or @ (X'7C') character.
+    """
     def __str__(self) -> str:
         """
-        Convert the name of the project to uppercase when converting it to a string.
-
-        Return:
-            str: The lowercase name of the project.
+        Return the enum name as upper case.
         """
         return self.name.upper()
 
     def string(self) -> str:
         """
-        Returns the string value contained in the tuple.
-        'online' for ONLINE and 'offline' for OFFLINE.
-
-        Return:
-            str: The string value contained in the tuple.
-                'online' for ONLINE and 'offline' for OFFLINE.
+        Returns the enum value as uppercase.
         """
         return self.value.upper()
 
+def get_random_passwd() -> str:
+    letters = string.ascii_uppercase
+    start = ''.join(random.choice(letters) for i in range(3))
+    middle = ''.join(str(random.randint(1,9)))
+    finish = ''.join(random.choice(letters) for i in range(4))
+    return f"{start}{middle}{finish}"
+
+def get_random_user(managed_user: ManagedUsers = None) -> str:
+    """A-Z, 0-9, #, $, or @."""
+    letters = string.ascii_uppercase
+    if managed_user is not None:
+        if managed_user.name == ManagedUsers.ZOS_BEGIN_WITH_AT_SIGN.name:
+            return "@" + ''.join(random.choice(letters) for i in range(7))
+        elif managed_user.name == ManagedUsers.ZOS_BEGIN_WITH_POUND.name:
+            return "#" + ''.join(random.choice(letters) for i in range(7))
+        elif managed_user.name == ManagedUsers.ZOS_RANDOM_SYMBOLS.name:
+            letters = string.ascii_uppercase
+            numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+            symbols = ['#', '$', '@',]
+
+            prefix = random.sample(letters,1)
+            alphas = random.sample(letters,2)
+            num = random.sample(numbers,2)
+            sym = random.sample(symbols,3)
+            alphas.extend(num)
+            alphas.extend(sym)
+
+            random.shuffle(alphas)
+
+            return str(prefix[0]) + ''.join([str(entry) for entry in alphas])
+
+    # All other cases can use any formatted user name, so random 8 letters is fine.
+    return ''.join(random.choice(letters) for i in range(8))
 
 def read_files_in_directory(directory: str = "~/.ssh", pattern: str = "*.pub") -> list:
     """Reads files in a directory that match the pattern and return file contents as a list entry."""
@@ -119,26 +133,37 @@ def read_files_in_directory(directory: str = "~/.ssh", pattern: str = "*.pub") -
     return file_contents_as_list
 
 def copy_ssh_key(user, passwd, host, key_path):
-    """Copy SSH key to a remote host using subprocess, note that some systems will not have sshpass installed."""
+    """
+    Copy SSH key to a remote host using subprocess.
+
+    Note:
+        This requires the host have sshpass installed.
+
+    See:
+        Method read_files_in_directory instead to copy the public keys.
+    """
 
     command = ["sshpass", "-p", f"{passwd}", "ssh-copy-id", "-o", "StrictHostKeyChecking=no", "-i", f"{key_path}", f"{user}@{host}"] #, "&>/dev/null"]
     result = subprocess.run(args=command, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, check=False)
 
     if result.returncode != 0:
-        raise Exception("Unable to copy public keys to remote host, check that sshpass is installed.") 
+        raise Exception("Unable to copy public keys to remote host, check that sshpass is installed.")
 
 def get_managed_user_name(ansible_zos_module, managed_user: ManagedUsers = None) -> str:
 
-    # TODO: Check if user exists and retry? Given this a randomly generated user, we can assume
-    # this won't be an issue and save the compute.
-    user = get_random_user()
+    # TODO: Check if user exists and retry?
+    # Given this a randomly generated user, we can assume this won't be an issue and save the compute.
+    user = get_random_user(managed_user)
 
-    # Generate the password to establish an password-less connection and use with RACF.
+    # print("USER " + user)
+    # Generate the password to establish a password-less connection and use with RACF.
     passwd = get_random_passwd()
 
+    # print("PASSWD " + user)
     # An existing user (model) will be used to access system specific values used to create a new user
     model_user = ansible_zos_module["options"]["user"]
 
+    # Access module user's TSO attributes for reuse
     results_tso_noracf = ansible_zos_module.all.shell(
             cmd=f"tsocmd LISTUSER {model_user} TSO NORACF"
     )
@@ -147,23 +172,31 @@ def get_managed_user_name(ansible_zos_module, managed_user: ManagedUsers = None)
     model_listuser_tso_attributes = list(results_tso_noracf.contacted.values())[0].get("stdout_lines")
 
     # Match the tso list results and place in variables (not all are used)
-    model_tso_acctnum = [v for v in model_listuser_tso_attributes if "ACCTNUM" in v][0].split('=')[1].strip() or ""
-    model_tso_proc = [v for v in model_listuser_tso_attributes if "PROC" in v][0].split('=')[1].strip() or ""
-    model_tso_size = [v for v in model_listuser_tso_attributes if "SIZE" in v][0].split('=')[1].strip() or ""
-    model_tso_maxsize = [v for v in model_listuser_tso_attributes if "MAXSIZE" in v][0].split('=')[1].strip() or ""
-    model_tso_unit = [v for v in model_listuser_tso_attributes if "UNIT" in v][0].split('=')[1].strip() or ""
-    model_tso_userdata = [v for v in model_listuser_tso_attributes if "USERDATA" in v][0].split('=')[1].strip() or ""
-    model_tso_command = [v for v in model_listuser_tso_attributes if "COMMAND" in v][0].split('=')[1].strip() or ""
+    model_tso_acctnum = [v for v in model_listuser_tso_attributes if "ACCTNUM" in v][0].split('=')[1].strip() or None
+    if not model_tso_acctnum:
+        raise Exception(f"Unable to determine the TSO Account number required for adding TSO {user}.")
 
-    # Print the matches for validation
-    print(f"model_tso_acctnum: [{model_tso_acctnum}]")
-    print(f"model_tso_proc: [{model_tso_proc}]")
-    print(f"model_tso_size: [{model_tso_size}]")
-    print(f"model_tso_maxsize: [{model_tso_maxsize}]")
-    print(f"model_tso_unit: [{model_tso_unit}]")
-    print(f"model_tso_userdata: [{model_tso_userdata}]")
-    print(f"model_tso_command: [{model_tso_command}]")
+    model_tso_proc = [v for v in model_listuser_tso_attributes if "PROC" in v][0].split('=')[1].strip() or None
+    if not model_tso_proc:
+        raise Exception(f"Unable to determine the TSO Proc required for adding TSO {user}.")
 
+    # These are currently not used when creating the user but are with considering minimally SIZE and MAXXSIZE
+    # model_tso_size = [v for v in model_listuser_tso_attributes if "SIZE" in v][0].split('=')[1].strip() or None
+    # model_tso_maxsize = [v for v in model_listuser_tso_attributes if "MAXSIZE" in v][0].split('=')[1].strip() or None
+    # model_tso_unit = [v for v in model_listuser_tso_attributes if "UNIT" in v][0].split('=')[1].strip() or ""
+    # model_tso_userdata = [v for v in model_listuser_tso_attributes if "USERDATA" in v][0].split('=')[1].strip() or ""
+    # model_tso_command = [v for v in model_listuser_tso_attributes if "COMMAND" in v][0].split('=')[1].strip() or ""
+
+    # Print the matches for validation, leaving this for debugging given this is new code.
+    # print(f"model_tso_acctnum: [{model_tso_acctnum}]")
+    # print(f"model_tso_proc: [{model_tso_proc}]")
+    # print(f"model_tso_size: [{model_tso_size}]")
+    # print(f"model_tso_maxsize: [{model_tso_maxsize}]")
+    # print(f"model_tso_unit: [{model_tso_unit}]")
+    # print(f"model_tso_userdata: [{model_tso_userdata}]")
+    # print(f"model_tso_command: [{model_tso_command}]")
+
+    # Access additional module user attributes for use in a new user.
     results_listuser = ansible_zos_module.all.shell(
             cmd=f"tsocmd LISTUSER {model_user}"
     )
@@ -174,35 +207,40 @@ def get_managed_user_name(ansible_zos_module, managed_user: ManagedUsers = None)
     # Match the list user results and place in variables (not all are used)
     model_owner = [v for v in model_listuser_attributes if "OWNER" in v][0].split('OWNER=')[1].split()[0].strip() or ""
 
-    # The shell command consisting of shell and tso operations to create a user.
+    # Collect the various public keys for use with the z/OS managed node, some accept only RSA and others ED25519
+    public_keys = read_files_in_directory("~/.ssh/", "*.pub")
+
+    # The command consisting of shell and tso operations to create a user.
     add_user_cmd = StringIO()
 
     # (1) Create group ANSIGRP for general identification of users and auto assign the UID
-    #     Success of the command yields 'Group ANSIGRP was assigned an OMVS GID value of 4'
+    #     Success of the command yields 'Group ANSIGRP was assigned an OMVS GID value of...'
     #     Failure of the command yields 'INVALID GROUP, ANSIGRP' with usually a RC 8 if the group exists.
     user_group = "ANSIGRP"
     add_user_cmd.write(f"tsocmd 'ADDGROUP {user_group} OMVS(AUTOGID)';")
     add_user_cmd.write(f"echo Create {user_group} RC=$?;")
-    public_keys = read_files_in_directory("~/.ssh/", "*.pub")
-    print(str(public_keys))
 
     # (2) Add the user to be owned by the model_owner. Expect a similar error when a new TSO userid is defined,
-    #     because that id can't receive deferred messages until the id is defined in SYS1.BRODCAST,  thus the SEND
+    #     because that id can't receive deferred messages until the id is defined in SYS1.BRODCAST, thus the SEND
     #     message fails to that user id.
     #       BROADCAST DATA SET NOT USABLE+
     #       I/O SYNAD ERROR
     add_user_cmd.write(f"tsocmd ADDUSER {user} DFLTGRP\\({user_group}\\) OWNER\\({model_owner}\\) NOPASSWORD TSO\\(ACCTNUM\\({model_tso_acctnum}\\) PROC\\({model_tso_proc}\\)\\) OMVS\\(HOME\\('/u/{user}'\\) PROGRAM\\('/bin/sh'\\) AUTOUID;")
-    add_user_cmd.write(f"echo ADDUSER {user} RC=$?;")
+    add_user_cmd.write(f"echo ADDUSER '{user}' RC=$?;")
     add_user_cmd.write(f"mkdir -p /u/{user}/.ssh;")
-    add_user_cmd.write(f"echo mkdir {user} RC=$?;")
+    add_user_cmd.write(f"echo mkdir '{user}' RC=$?;")
     add_user_cmd.write(f"umask 0022;")
     add_user_cmd.write(f"touch /u/{user}/.ssh/authorized_keys;")
+    add_user_cmd.write(f"echo touch authorized_keys RC=$?;")
     add_user_cmd.write(f"chown -R {user} /u/{user};")
-    add_user_cmd.write(f"echo chown {user} RC=$?;")
+    add_user_cmd.write(f"echo chown '{user}' RC=$?;")
     for pub_key in public_keys:
         add_user_cmd.write(f"echo {pub_key} >> /u/{user}/.ssh/authorized_keys;")
-    add_user_cmd.write(f"tsocmd ALTUSER {user} PASSWORD\\({passwd}\\) NOEXPIRED")
-    print(f"add_user_cmd [{add_user_cmd.getvalue()}]")
+    add_user_cmd.write(f"tsocmd ALTUSER {user} PASSWORD\\({passwd}\\) NOEXPIRED;")
+    add_user_cmd.write(f"echo ALTUSER '{user}' RC=$?;")
+
+    # Print the command being used to add the new user. Commented out for now.
+    # print(f"add_user_cmd [{add_user_cmd.getvalue()}]")
 
     results_add_user_cmd = ansible_zos_module.all.shell(
             cmd=f"{add_user_cmd.getvalue()}"
@@ -210,24 +248,50 @@ def get_managed_user_name(ansible_zos_module, managed_user: ManagedUsers = None)
 
     # Extract the new user stdout lines for evaluation
     add_user_attributes = list(results_add_user_cmd.contacted.values())[0].get("stdout_lines")
-    print("results_add_user_cmd.contacted.values() " + str(results_add_user_cmd.contacted.values()))
-    print("add_user_attributes: " + str(add_user_attributes))
-    # Match the new user return codes
-    assigned_omvs_uid = True if [v for v in add_user_attributes if f"User {user} was assigned an OMVS UID value" in v] else False
+
+    # Print stdout from add user.
+    # print("add_user_attributes stdout: " + str(add_user_attributes))
+
+    # Because this is a tsocmd run through shell, any user with a $ will be expanded and thus truncated so you can't change
+    # that behavior since its happening on the managed node, solution is to match a shorter pattern without the user. 
+    is_assigned_omvs_uid = True if [v for v in add_user_attributes if f"was assigned an OMVS UID value" in v] else False
+    if not is_assigned_omvs_uid:
+        raise Exception(f"Unable to create OMVS UID.")
+
     create_group_rc = [v for v in add_user_attributes if f"Create {user_group} RC=" in v][0].split('=')[1].strip() or None
+    if not create_group_rc or int(create_group_rc[0]) > 8:
+        raise Exception(f"Unable to create user group {user_group}.")
+
     add_user_rc = [v for v in add_user_attributes if f"ADDUSER {user} RC=" in v][0].split('=')[1].strip() or None
+    if not add_user_rc or int(add_user_rc[0]) > 0:
+        raise Exception(f"Unable to ADDUSER {user}.")
+
     mkdir_rc = [v for v in add_user_attributes if f"mkdir {user} RC=" in v][0].split('=')[1].strip() or None
+    if not mkdir_rc or int(mkdir_rc[0]) > 0:
+        raise Exception(f"Unable to make home directories for {user}.")
+
+    authorized_keys_rc = [v for v in add_user_attributes if f"touch authorized_keys RC=" in v][0].split('=')[1].strip() or None
+    if not authorized_keys_rc or int(authorized_keys_rc[0]) > 0:
+        raise Exception(f"Unable to make authorized_keys file for {user}.")
+
     chown_rc = [v for v in add_user_attributes if f"chown {user} RC=" in v][0].split('=')[1].strip() or None
+    if not chown_rc or int(chown_rc[0]) > 0:
+        raise Exception(f"Unable to change ownership of home directory for {user}.")
+
+    altuser_rc = [v for v in add_user_attributes if f"ALTUSER {user} RC=" in v][0].split('=')[1].strip() or None
+    if not altuser_rc or int(altuser_rc[0]) > 0:
+        raise Exception(f"Unable to update password for {user}.")
 
     # Print the RCs for validation
-    print(f"assigned_omvs_uid: [{assigned_omvs_uid}]")
-    print(f"create_group_rc: [{create_group_rc}]")
-    print(f"add_user_rc: [{add_user_rc}]")
-    print(f"mkdir_rc: [{mkdir_rc}]")
-    print(f"chown_rc: [{chown_rc}]")
+    # print(f"is_assigned_omvs_uid: [{is_assigned_omvs_uid}]")
+    # print(f"create_group_rc: [{create_group_rc}]")
+    # print(f"add_user_rc: [{add_user_rc}]")
+    # print(f"mkdir_rc: [{mkdir_rc}]")
+    # print(f"authorized_keys_rc: [{authorized_keys_rc}]")
+    # print(f"chown_rc: [{chown_rc}]")
 
     # remote_host = ansible_zos_module["options"]["inventory"].replace(",", "")
-    return None #operations[managed_user.name](ansible_zos_module)
+    # return None #operations[managed_user.name](ansible_zos_module)
 
     # These will serve the general purpose of our test cases
 def create_user_zoau_limited_access_opercmd(ansible_zos_module) -> str:

86725900931245dda046d6912ff5686acd40e3a9
André Marcel Gutiérrez Benítez <amgutierrezbenitez@hotmail.com>
2024-11-04 08:41:49 -0600

    Fix lint

--- plugins/action/zos_fetch.py
+++ plugins/action/zos_fetch.py
@@ -156,7 +156,7 @@ class ActionModule(ActionBase):
             member_name = src[src.find("(") + 1: src.find(")")]
         src = self._connection._shell.join_path(src)
         src = self._remote_expand_user(src)
-    
+
         # To manage relative paths we verify and add the current directory
         # Required to be cast to str
         if not (dest.startswith("/")):

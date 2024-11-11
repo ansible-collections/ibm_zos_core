6633968c20f825c4a3b1e5bfb02061c9d349b2e9
André Marcel Gutiérrez Benítez <amgutierrezbenitez@hotmail.com>
2024-11-11 09:51:49 -0600

    Generate file

---
+++ tests/functional/modules/test_zos_zfs_resize_func.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) IBM Corporation 2024
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import, division, print_function
+
+import os
+import tempfile
+import pytest
+
+from ibm_zos_core.tests.helpers.dataset import (
+    get_tmp_ds_name,
+    get_random_q,
+)
+from ibm_zos_core.tests.helpers.utils import get_random_file_name
+
+__metaclass__ = type
+
+def set_environment():

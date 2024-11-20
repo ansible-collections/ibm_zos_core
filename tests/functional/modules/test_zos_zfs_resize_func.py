4158d40e30115cde7ee7a865cfd7a5f8050d4305
André Marcel Gutiérrez Benítez <amgutierrezbenitez@hotmail.com>
2024-11-20 12:54:33 -0600

    Add first two basic test cases

--- tests/functional/modules/test_zos_zfs_resize_func.py
+++ tests/functional/modules/test_zos_zfs_resize_func.py
@@ -14,16 +14,73 @@
 
 from __future__ import absolute_import, division, print_function
 
-import os
-import tempfile
 import pytest
 
-from ibm_zos_core.tests.helpers.dataset import (
-    get_tmp_ds_name,
-    get_random_q,
-)
-from ibm_zos_core.tests.helpers.utils import get_random_file_name
+from ibm_zos_core.tests.helpers.dataset import get_tmp_ds_name
 
 __metaclass__ = type
 
-def set_environment():
+def make_temp_folder(hosts):
+    """ Create temporary file on z/OS system and return the path """
+    tempfile_name = ""
+    results = hosts.all.tempfile(state="directory")
+    for result in results.contacted.values():
+        tempfile_name = result.get("path", "")
+    return tempfile_name
+
+def set_environment(ansible_zos_module, ds_name, space=1, space_type='m'):
+    hosts = ansible_zos_module
+
+    hosts.all.zos_data_set(name=ds_name, type="zfs", space_primary=space, space_type=space_type)
+    temp_dir_name = make_temp_folder(hosts=hosts)
+    hosts.all.command(
+                cmd="usr/sbin/mount -t zfs -f {0} {1}".format(
+                    ds_name, temp_dir_name
+                )
+            )
+    hosts.all.command(
+                cmd="head -c 1000000 /dev/urandom > {0}/test.txt".format(
+                    temp_dir_name
+                )
+    )
+
+    return temp_dir_name
+
+def clean_up_environment(hosts, ds_name, temp_dir_name):
+    hosts.all.command(cmd=f"usr/sbin/unmount {temp_dir_name}")
+    hosts.all.zos_data_set(name=ds_name, state="absent")
+    hosts.all.file(path=temp_dir_name, state="absent")
+
+def test_grow_operation(ansible_zos_module):
+    hosts = ansible_zos_module
+    ds_name = get_tmp_ds_name()
+    mount_folder = ""
+    size = 2500
+    try:
+        mount_folder = set_environment(ansible_zos_module=hosts, ds_name=ds_name)
+        results = hosts.all.zos_zfs_resize(target=ds_name, size=size)
+        for result in results.contacted.values():
+            assert result.get('target') == ds_name
+            assert result.get('mount_target') == "/SYSTEM" + mount_folder
+            assert result.get('rc') == 0
+            assert "grown" in result.get('stdout')
+            assert result.get('new_size') >= size
+    finally:
+        clean_up_environment(hosts=hosts, ds_name=ds_name, temp_dir_name=mount_folder)
+
+def test_shrink_operation(ansible_zos_module):
+    hosts = ansible_zos_module
+    ds_name = get_tmp_ds_name()
+    mount_folder = ""
+    size = 1400
+    try:
+        mount_folder = set_environment(ansible_zos_module=hosts, ds_name=ds_name)
+        results = hosts.all.zos_zfs_resize(target=ds_name, size=size)
+        for result in results.contacted.values():
+            assert result.get('target') == ds_name
+            assert result.get('mount_target') == "/SYSTEM" + mount_folder
+            assert result.get('rc') == 0
+            assert "shrunk" in result.get('stdout')
+            assert result.get('new_size') <= size
+    finally:
+        clean_up_environment(hosts=hosts, ds_name=ds_name, temp_dir_name=mount_folder)

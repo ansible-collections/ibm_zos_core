ce7ee5eb728815da9e7c3c76602cf1fe4e04a9e7
André Marcel Gutiérrez Benítez <amgutierrezbenitez@hotmail.com>
2025-01-24 12:06:37 -0600

    Add copy

--- plugins/modules/zos_copy.py
+++ plugins/modules/zos_copy.py
@@ -3158,49 +3158,52 @@ def normalize_line_endings(src, encoding=None):
 
 
 def data_set_locked(dataset_name):
-     """
-     Checks if a data set is in use and therefore locked (DISP=SHR), which
-     is often caused by a long running task. Returns a boolean value to indicate the data set status.
-     Parameters
-     ----------
-     dataset_name (str):
-         The data set name used to check if there is a lock.
-     Returns
-     -------
-     bool
-         True if the data set is locked, or False if the data set is not locked.
-     Raises
-     ------
-     CopyOperationError
-         When the user does not have Universal Access Authority to
-         ZOAU SAF Profile 'MVS.MCSOPER.ZOAU' and SAF Class OPERCMDS.
-     """
-     # Using operator command "D GRS,RES=(*,{dataset_name})" to detect if a data set
-     # is in use, when a data set is in use it will have "EXC/SHR and SHARE"
-     # in the result with a length greater than 4.
-     result = dict()
-     result["stdout"] = []
-     command_dgrs = "D GRS,RES=(*,{0})".format(dataset_name)
+    """
+    Checks if a data set is in use and therefore locked (DISP=SHR), which
+    is often caused by a long running task. Returns a boolean value to indicate the data set status.
 
-     try:
-         response = opercmd.execute(command=command_dgrs)
-         stdout = response.stdout_response
+    Parameters
+    ----------
+    dataset_name (str):
+        The data set name used to check if there is a lock.
 
-         if stdout is not None:
-             for out in stdout.split("\n"):
-                 if out:
-                     result["stdout"].append(out)
-         if len(result["stdout"]) <= 4 and "NO REQUESTORS FOR RESOURCE" in stdout:
-             return False
+    Returns
+    -------
+    bool
+        True if the data set is locked, or False if the data set is not locked.
 
-         return True
-     except zoau_exceptions.ZOAUException as copy_exception:
-         raise CopyOperationError(
-             msg="Unable to determine if the dest {0} is in use.".format(dataset_name),
-                 rc=copy_exception.response.rc,
-                 stdout=copy_exception.response.stdout_response,
-                 stderr=copy_exception.response.stderr_response
-         )
+    Raises
+    ------
+    CopyOperationError
+        When the user does not have Universal Access Authority to
+        ZOAU SAF Profile 'MVS.MCSOPER.ZOAU' and SAF Class OPERCMDS.
+    """
+    # Using operator command "D GRS,RES=(*,{dataset_name})" to detect if a data set
+    # is in use, when a data set is in use it will have "EXC/SHR and SHARE"
+    # in the result with a length greater than 4.
+    result = dict()
+    result["stdout"] = []
+    command_dgrs = "D GRS,RES=(*,{0})".format(dataset_name)
+
+    try:
+        response = opercmd.execute(command=command_dgrs)
+        stdout = response.stdout_response
+
+        if stdout is not None:
+            for out in stdout.split("\n"):
+                if out:
+                    result["stdout"].append(out)
+        if len(result["stdout"]) <= 4 and "NO REQUESTORS FOR RESOURCE" in stdout:
+            return False
+
+        return True
+    except zoau_exceptions.ZOAUException as copy_exception:
+        raise CopyOperationError(
+            msg="Unable to determine if the dest {0} is in use.".format(dataset_name),
+                rc=copy_exception.response.rc,
+                stdout=copy_exception.response.stdout_response,
+                stderr=copy_exception.response.stderr_response
+        )
 
 
 def run_module(module, arg_def):

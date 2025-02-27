f74833567954fd7461d6b642e6d7c0655872ea2e
surendrababuravella <39149274+surendrababuravella@users.noreply.github.com>
2025-02-28 00:23:55 +0530

    Update zos_copy.py

--- plugins/modules/zos_copy.py
+++ plugins/modules/zos_copy.py
@@ -1485,6 +1485,45 @@ class CopyHandler(object):
                 stderr=to_native(err)
             )
 
+    def remove_cr_endings(self, src):
+        """Creates a temporary file with the same content as src but without
+        carriage returns.
+
+        Parameters
+        ----------
+        src : str
+            Path to a USS source file.
+
+        Returns
+        -------
+        str
+            Path to the temporary file created.
+
+        Raises
+        ------
+        CopyOperationError
+            If the conversion fails.
+        """
+        try:
+            fd, converted_src = tempfile.mkstemp(dir=os.environ['TMPDIR'])
+            os.close(fd)
+            #defining 32 MB chunk size for reading large files efficiently
+            chunk_size = 32 * 1024 * 1024
+            with open(converted_src, "wb") as converted_file:
+                with open(src, "rb") as src_file:
+                    chunk = src_file.read(chunk_size)
+                    while chunk:
+                        # In IBM-037, \r is the byte 0d.
+                        converted_file.write(chunk.replace(b'\x0d', b''))
+                        chunk = src_file.read(chunk_size)
+
+            return converted_src
+        except Exception as err:
+            raise CopyOperationError(
+                msg="Error while trying to convert EOL sequence for source.",
+                stderr=to_native(err)
+            )
+
 
 class USSCopyHandler(CopyHandler):
     def __init__(
@@ -3727,7 +3766,7 @@ def run_module(module, arg_def):
             if src_ds_type == "USS" and not is_binary and not executable:
                 new_src = conv_path or src
                 if os.path.isfile(new_src):
-                    conv_path = copy_handler.create_temp_with_lf_endings(new_src)
+                    conv_path = copy_handler.remove_cr_endings(new_src)
             uss_copy_handler = USSCopyHandler(
                 module,
                 is_binary=is_binary,

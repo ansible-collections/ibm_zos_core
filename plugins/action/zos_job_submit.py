7c81ca14316b0359691394b84a9fbb0b9d7b630c
Mayank Mani <mayankmanics22@gmail.com>
2025-03-03 18:33:23 +0530

    Resolving sanity issue

--- plugins/action/zos_job_submit.py
+++ plugins/action/zos_job_submit.py
@@ -126,9 +126,9 @@ class ActionModule(ActionBase):
 
                 source_full = rendered_file
                 if os.path.exists(rendered_file):
-                   with open(rendered_file, 'r') as file:
-                      rendered_content = file.read()
-                      display.vvv(u"Full rendered template content for {0}:\n{1}".format(os.path.basename(source), rendered_content), host=self._play_context.remote_addr)
+                    with open(rendered_file, 'r') as file:
+                        rendered_content = file.read()
+                        display.vvv(u"Full rendered template content for {0}:\n{1}".format(os.path.basename(source), rendered_content), host=self._play_context.remote_addr)
                 else:
                     display.vvv(u"Rendered template file {0} does not exist.".format(rendered_file))
 

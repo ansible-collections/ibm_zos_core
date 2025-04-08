45267d2223c9dee44f80c117b7257e377a69fcca
André Marcel Gutiérrez Benítez <amgutierrezbenitez@hotmail.com>
2025-04-08 16:25:14 -0600

    Try to order

--- tests/helpers/volumes.py
+++ tests/helpers/volumes.py
@@ -213,8 +213,7 @@ def get_volume_and_unit(ansible_zos_module, path):
                     shortest = 2
                     if len(datasets) < 30:
                         print(len(datasets), vol_w_info[3])
-                        if len(datasets) <= shortest:
-                            shortest = len(datasets)
+                        if len(datasets) >= shortest:
                             priv_online.insert(0, [vol_w_info[3], vol_w_info[0]])
                         elif len(datasets) >= largest:
                             largest = len(datasets)

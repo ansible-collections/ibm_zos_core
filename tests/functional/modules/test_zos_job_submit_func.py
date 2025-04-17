fe9ca43dfed00e9adce6af820e38155c087b503b
surendrababuravella <39149274+surendrababuravella@users.noreply.github.com>
2025-04-17 10:49:58 +0530

    Update test_zos_job_submit_func.py

--- tests/functional/modules/test_zos_job_submit_func.py
+++ tests/functional/modules/test_zos_job_submit_func.py
@@ -622,8 +622,9 @@ def test_job_submit_local_badjcl(ansible_zos_module):
     results = hosts.all.zos_job_submit(src=tmp_file.name, location="local", wait_time_s=10)
 
     for result in results.contacted.values():
+        # Expecting: The job completion code (CC) was not in the job log....."
         assert result.get("changed") is False
-        assert result.get("failed") is True
+        assert re.search(r'completion code', repr(result.get("msg")))
 
 
 def test_job_submit_pds_volume(ansible_zos_module, volumes_on_systems):

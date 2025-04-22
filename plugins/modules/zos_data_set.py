897ea2ddb4806dbd36c01b2d8cf1d66f401019ac
Mayank Mani <mayankmanics22@gmail.com>
2025-04-23 00:02:41 +0530

    Update plugins/modules/zos_data_set.py
    Co-authored-by: Alex Moreno <iamorenosoto@gmail.com>

--- plugins/modules/zos_data_set.py
+++ plugins/modules/zos_data_set.py
@@ -104,7 +104,7 @@ options:
       - >
         If I(state=uncataloged) and the data set is found, the data set is uncataloged,
         module completes successfully with I(changed=True).
-        - >
+      - >
         If I(state=present), the data set is already cataloged and I(volumes) is provided,
         the module will compare the volumes where it is cataloged against the provided I(volumes).
         If they don't match, the module will fail with an error indicating the data set is cataloged

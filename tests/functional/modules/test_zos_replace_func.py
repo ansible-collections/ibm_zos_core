804bcafa0de817316cd556635338282f6e6ced97
André Marcel Gutiérrez Benítez <amgutierrezbenitez@hotmail.com>
2025-05-28 13:00:09 -0600

    Add multiline regex

--- tests/functional/modules/test_zos_replace_func.py
+++ tests/functional/modules/test_zos_replace_func.py
@@ -280,6 +280,12 @@ ZOAU_ROOT=/usr/lpp/zoautil/v100
 export NEW_ZOAU_ROOT
 export NEW__BPXK_AUTOCVT"""
 
+TEST_MATCH_MULTIPLE_LINES = """PATH=/usr/lpp/zoautil/v100/bin:/usr/lpp/rsusr/ported/bin:/bin:/var/bin
+export PATH
+ZOAU_ROOT=/usr/lpp/zoautil/v100
+export ZOAU_ROOT
+export _BPXK_AUTOCVT"""
+
 #####################
 #  Set up testing
 #####################
@@ -304,10 +310,10 @@ def remove_uss_environment(ansible_zos_module, file):
 def set_ds_environment(ansible_zos_module, temp_file, ds_name, ds_type, content):
     hosts = ansible_zos_module
     hosts.all.shell(cmd=f"echo \"{content}\" > {temp_file}")
-    hosts.all.zos_data_set(name=ds_name, type=ds_type)
+    hosts.all.shell(cmd=f"dtouch {ds_name} -t {ds_type}")
     if ds_type in ["pds", "pdse"]:
         ds_full_name = ds_name + "(MEM)"
-        hosts.all.zos_data_set(name=ds_full_name, state="present", type="member")
+        hosts.all.shell(cmd=f"decho '' '{ds_full_name}' ")
         cmd_str = f"cp -CM {quote(temp_file)} \"//'{ds_full_name}'\""
     else:
         ds_full_name = ds_name
@@ -318,7 +324,7 @@ def set_ds_environment(ansible_zos_module, temp_file, ds_name, ds_type, content)
 
 def remove_ds_environment(ansible_zos_module, ds_name):
     hosts = ansible_zos_module
-    hosts.all.zos_data_set(name=ds_name, state="absent")
+    hosts.all.shell(cmd=f"drm -F {ds_name}")
 
 #####################
 # Testing
@@ -870,6 +876,27 @@ def test_uss_backref(ansible_zos_module):
         results = hosts.all.shell(cmd="cat {0}".format(params["target"]))
         for result in results.contacted.values():
             assert result.get("stdout") == TEST_BACKREF
+    finally:
+        remove_uss_environment(ansible_zos_module, full_path)
+
+def test_uss_match_multiple_lines(ansible_zos_module):
+    hosts = ansible_zos_module
+    params = {
+        "regexp":"(?s)^(if .*?\nthen\n.*?\n.*?\n.*?\n)",
+    }
+    full_path = get_random_file_name(dir=TMP_DIRECTORY)
+    content = TEST_CONTENT
+    try:
+        set_uss_environment(ansible_zos_module, content, full_path)
+        params["target"] = full_path
+        results = hosts.all.zos_replace(**params)
+        for result in results.contacted.values():
+            assert result.get("changed") == True
+            assert result.get("target") == full_path
+            assert result.get("found") == 1
+        results = hosts.all.shell(cmd="cat {0}".format(params["target"]))
+        for result in results.contacted.values():
+            assert result.get("stdout") == TEST_MATCH_MULTIPLE_LINES
     finally:
         remove_uss_environment(ansible_zos_module, full_path)
 
@@ -1444,6 +1471,31 @@ def test_ds_backref(ansible_zos_module, dstype):
         results = hosts.all.shell(cmd="cat \"//'{0}'\" ".format(params["target"]))
         for result in results.contacted.values():
             assert result.get("stdout") == TEST_BACKREF
+    finally:
+        remove_ds_environment(ansible_zos_module, ds_name)
+
+@pytest.mark.ds
+@pytest.mark.parametrize("dstype", DS_TYPE)
+def test_ds_match_multiple_lines(ansible_zos_module, dstype):
+    hosts = ansible_zos_module
+    ds_type = dstype
+    params = {
+        "regexp":"(?s)^(if .*?\nthen\n.*?\n.*?\n.*?\n)",
+    }
+    ds_name = get_tmp_ds_name()
+    temp_file = get_random_file_name(dir=TMP_DIRECTORY)
+    content = TEST_CONTENT
+    try:
+        ds_full_name = set_ds_environment(ansible_zos_module, temp_file, ds_name, ds_type, content)
+        params["target"] = ds_full_name
+        results = hosts.all.zos_replace(**params)
+        for result in results.contacted.values():
+            assert result.get("changed") == True
+            assert result.get("target") == ds_full_name
+            assert result.get("found") == 1
+        results = hosts.all.shell(cmd="cat \"//'{0}'\" ".format(params["target"]))
+        for result in results.contacted.values():
+            assert result.get("stdout") == TEST_MATCH_MULTIPLE_LINES
     finally:
         remove_ds_environment(ansible_zos_module, ds_name)
 

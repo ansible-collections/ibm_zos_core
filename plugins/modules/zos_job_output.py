edaf3e0b82e94b571dfe656539342a06584e5539
Rohitcodes28 <rohitashgoyal509@gmail.com>
2025-07-09 16:14:37 +0530

    adding sysin support

--- plugins/modules/zos_job_output.py
+++ plugins/modules/zos_job_output.py
@@ -33,6 +33,7 @@ description:
     like "*".
   - If there is no ddname, or if ddname="?", output of all the ddnames under
     the given job will be displayed.
+  - If user needs input like SYSIN, input parameter should be set true.
 version_added: "1.0.0"
 author:
   - "Jack Ho (@jacklotusho)"
@@ -60,6 +61,11 @@ options:
       - Data definition name (show only this DD on a found job).
         (e.g "JESJCL", "?")
     type: str
+    required: false
+  input:
+    description:
+      - Input DD SYSIN (show only this DD on a found job).
+    type: bool
     required: false
 
 attributes:
@@ -496,6 +502,7 @@ def run_module():
         job_name=dict(type="str", required=False),
         owner=dict(type="str", required=False),
         ddname=dict(type="str", required=False),
+        input=dict(type="bool", required=False, default=False),
     )
 
     module = AnsibleModule(argument_spec=module_args, supports_check_mode=True)
@@ -505,6 +512,7 @@ def run_module():
         job_name=dict(type="job_identifier", required=False),
         owner=dict(type="str", required=False),
         ddname=dict(type="str", required=False),
+        input=dict(type="bool", required=False, default=False),
     )
 
     try:
@@ -521,13 +529,14 @@ def run_module():
     job_name = module.params.get("job_name")
     owner = module.params.get("owner")
     ddname = module.params.get("ddname")
+    sysin = module.params.get("input")
 
     if not job_id and not job_name and not owner:
         module.fail_json(msg="Please provide a job_id or job_name or owner")
 
     try:
         results = {}
-        results["jobs"] = job_output(job_id=job_id, owner=owner, job_name=job_name, dd_name=ddname)
+        results["jobs"] = job_output(job_id=job_id, owner=owner, job_name=job_name, dd_name=ddname, sysin=sysin)
         results["changed"] = False
     except zoau_exceptions.JobFetchException as fetch_exception:
         module.fail_json(

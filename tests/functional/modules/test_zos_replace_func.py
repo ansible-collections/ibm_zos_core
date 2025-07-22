e5b76fdee74680853707519cab97951166cd48a5
André Marcel Gutiérrez Benítez <68956970+AndreMarcel99@users.noreply.github.com>
2025-07-22 14:54:56 -0600

    Update test_zos_replace_func.py

--- tests/functional/modules/test_zos_replace_func.py
+++ tests/functional/modules/test_zos_replace_func.py
@@ -380,7 +380,7 @@ def test_uss_after_replace(ansible_zos_module):
             assert result.get("found") == 2
         results = hosts.all.shell(cmd="cat {0}".format(params["target"]))
         for result in results.contacted.values():
-            print("result:")
+            print("result::")
             print(result)
             assert result.get("stdout") == TEST_AFTER_REPLACE
     finally:

76891026cab71da145764831adfe9e2e92e8ffa7
André Marcel Gutiérrez Benítez <68956970+AndreMarcel99@users.noreply.github.com>
2025-07-22 15:09:48 -0600

    Update test_zos_replace_func.py

--- tests/functional/modules/test_zos_replace_func.py
+++ tests/functional/modules/test_zos_replace_func.py
@@ -356,6 +356,7 @@ def test_uss_after(ansible_zos_module):
             assert result.get("found") == 2
         results = hosts.all.shell(cmd="cat {0}".format(params["target"]))
         for result in results.contacted.values():
+            print(result)
             assert result.get("stdout") == TEST_AFTER
     finally:
         remove_uss_environment(ansible_zos_module, full_path)

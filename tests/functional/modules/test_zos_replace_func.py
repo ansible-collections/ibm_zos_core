2ad64988023362fea8fcf4fc4700c4ebcde7e053
André Marcel Gutiérrez Benítez <68956970+AndreMarcel99@users.noreply.github.com>
2025-09-03 16:29:34 -0600

    Update test_zos_replace_func.py

--- tests/functional/modules/test_zos_replace_func.py
+++ tests/functional/modules/test_zos_replace_func.py
@@ -350,7 +350,7 @@ def test_uss_after(ansible_zos_module):
         params["target"] = full_path
         results = hosts.all.zos_replace(**params)
         for result in results.contacted.values():
-            print("result::")
+            print("result:")
             print(result)
             assert result.get("changed") == True
             assert result.get("target") == full_path

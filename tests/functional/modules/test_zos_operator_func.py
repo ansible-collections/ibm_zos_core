4c0a9832c7f5c84d3196d638c2e241ec89e65a52
Rohitcodes28 <rohitashgoyal509@gmail.com>
2025-09-15 18:00:08 +0530

    test case sequence change

--- tests/functional/modules/test_zos_operator_func.py
+++ tests/functional/modules/test_zos_operator_func.py
@@ -61,6 +61,40 @@ INVENTORY = """all:
       ansible_user: {2}
       ansible_python_interpreter: {3}"""
 
+def test_zos_operator_parallel_terminal(path):
+    with open(path, 'r') as file:
+        enviroment = yaml.safe_load(file)
+    ssh_key = enviroment["ssh_key"]
+    hosts = enviroment["host"].upper()
+    user = enviroment["user"].upper()
+    python_path = enviroment["python_path"]
+    cut_python_path = python_path[:python_path.find('/bin')].strip()
+    zoau = enviroment["environment"]["ZOAU_ROOT"]
+    python_version = cut_python_path.split('/')[2]
+
+    try:
+        playbook = "playbook.yml"
+        inventory = "inventory.yml"
+        os.system("echo {0} > {1}".format(quote(PARALLEL_RUNNING.format(
+            zoau,
+            cut_python_path,
+            python_version
+        )), playbook))
+        os.system("echo {0} > {1}".format(quote(INVENTORY.format(
+            hosts,
+            ssh_key,
+            user,
+            python_path
+        )), inventory))
+        command = "(ansible-playbook -i {0} {1}) & (ansible-playbook -i {0} {1})".format(
+            inventory,
+            playbook,
+        )
+        stdout = os.system(command)
+        assert stdout == 0
+    finally:
+        os.remove("inventory.yml")
+        os.remove("playbook.yml")
 
 def test_zos_operator_various_command(ansible_zos_module):
     test_data = [
@@ -198,38 +232,3 @@ def test_response_come_back_complete(ansible_zos_module):
         last_line = len(stdout)
         assert "HASP646" in stdout[last_line - 1]
 
-
-def test_zos_operator_parallel_terminal(path):
-    with open(path, 'r') as file:
-        enviroment = yaml.safe_load(file)
-    ssh_key = enviroment["ssh_key"]
-    hosts = enviroment["host"].upper()
-    user = enviroment["user"].upper()
-    python_path = enviroment["python_path"]
-    cut_python_path = python_path[:python_path.find('/bin')].strip()
-    zoau = enviroment["environment"]["ZOAU_ROOT"]
-    python_version = cut_python_path.split('/')[2]
-
-    try:
-        playbook = "playbook.yml"
-        inventory = "inventory.yml"
-        os.system("echo {0} > {1}".format(quote(PARALLEL_RUNNING.format(
-            zoau,
-            cut_python_path,
-            python_version
-        )), playbook))
-        os.system("echo {0} > {1}".format(quote(INVENTORY.format(
-            hosts,
-            ssh_key,
-            user,
-            python_path
-        )), inventory))
-        command = "(ansible-playbook -i {0} {1}) & (ansible-playbook -i {0} {1})".format(
-            inventory,
-            playbook,
-        )
-        stdout = os.system(command)
-        assert stdout == 0
-    finally:
-        os.remove("inventory.yml")
-        os.remove("playbook.yml")

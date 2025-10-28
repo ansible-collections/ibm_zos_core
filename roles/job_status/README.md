77ace04da998b09ec617f51f5a1bc4e919f68c6b
André Marcel Gutiérrez Benítez <Andre.Marcel.Gutierrez@ibm.com>
2025-10-27 18:29:38 -0600

    Add test case

---
+++ roles/job_status/README.md
@@ -0,0 +1,9 @@
+# ibm.ibm_zos_core.job_status
+
+This role gathers job status and if the jos is active about a giving a job_id.
+
+## Role Variables
+
+Tue only variable required is:
+
+- `job_id`: ID of the job to query status and if is active or not.
---
+++ tests/functional/roles/test_role_job_status.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) IBM Corporation 2025
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import, division, print_function
+
+import pytest
+__metaclass__ = type
+
+def test_my_role_with_vars(ansible_zos_module):
+    hosts = ansible_zos_module
+
+    jobs = hosts.all.zos_job_query(job_id="*", owner="*")
+    for job in jobs.contacted.values():
+        job_id = job.gets("jobs")[0].get("job_id")
+
+    hosts.all.set_fact(job_id=job_id)
+    results = hosts.all.include_role(name="job_status")
+    for result in results.contacted.values():
+        assert result.get("msg").get("job_active") is True
+        assert result.get("msg").get("job_status") is not None

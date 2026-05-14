511bcccdc54723a62eb033d09d726938bf4eae4b
Rohitash Goyal <rohitashgoyal509@gmail.com>
2026-05-14 19:44:40 +0530

    [Role][2168] support_mode role to gather all facts (#2392)
    * role addition
    
    * zos managed file changes without apf
    
    * testing removing apf
    
    * managed node additions
    
    * adding doc for role
    
    * managed node
    
    * apf bits addition
    
    * adding test case
    
    * changelog and readme
    
    * readme update
    
    * adding no log functionality
    
    * document update
    
    * Rename Readme
    
    * review comments
    
    * test changes
    
    * controller and managed both working
    
    * updating role
    
    * review comments incorporation
    
    * review comments
    
    * review comments
    
    * review comments incorporation
    
    * review comments
    
    * Updated the roles details
    
    * update python path used for localhost exec, add inventory hostname to log file name
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * update output dir var to target /artifacts dir in aap for easier use
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * update role var names for clarity, add python interpreter override for localhost, edit zoau/pyz paths regex, update docs
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * update for consistency
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * update changelog
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * update var names, udpate examples
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * fix sanity/linter warnings
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * update var names, udpate examples
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * rename vars per ansible-lint var-naming[no-role-prefix] guidelines
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * add required meta dir and content
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * fix line lengths
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * convert shell to command where reasonable
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * remove extension to docs file
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * replace 'controller' with 'control' or 'control node' for accurate terminology
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * update python regex checker to more generic pyz
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * update python regex checker to accomodate single digit release versions
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * fix python regex
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * add task to ensure target dir exists
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * refactor tests file to update var names and handle other changes made to the role
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * remove colons from docs
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    * remove colons from docs
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    
    ---------
    
    Signed-off-by: Ketan Kelkar <ktnklkr@gmail.com>
    Co-authored-by: Fernando Flores <fernandofloresdev@gmail.com>
    Co-authored-by: Ketan Kelkar <ktnklkr@gmail.com>
    Co-authored-by: surendrababuravella <39149274+surendrababuravella@users.noreply.github.com>

---
+++ tests/functional/roles/test_role_support_mode.py
@@ -0,0 +1,111 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) IBM Corporation 2026
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from __future__ import absolute_import, division, print_function
+
+import pytest
+__metaclass__ = type
+
+TMP_DIRECTORY = "/tmp/pytest-support-mode-output"
+
+def assert_module_did_not_fail(results):
+    for result in results.contacted.values():
+        assert (
+            result.get("failed", False) is not True
+            and not result.get("exception", "")
+            and "error" not in result.get("msg", "").lower()
+            )
+
+def assert_controller_file_exists(hosts, path):
+    results = hosts.localhost.stat(path=path)
+    for result in results.contacted.values():
+        assert result.get("stat").get("exists") is True
+
+def assert_controller_file_does_not_exist(hosts, path):
+    results = hosts.localhost.stat(path=path)
+    for result in results.contacted.values():
+        assert result.get("stat").get("exists") is False
+
+def setup_test_directory(hosts, path):
+    hosts.localhost.file(path=path, state="absent")
+    hosts.localhost.file(path=path, state="directory")
+
+
+def cleanup_test_directory(hosts, path):
+    hosts.localhost.file(path=path, state="absent")
+
+
+def test_gather_diagnostics_role_default(ansible_zos_module):
+    hosts = ansible_zos_module
+    control_report = f"{TMP_DIRECTORY}/gather_diagnostics_report_control.yml"
+    # Get the inventory hostname from the hosts object to build the diagnostic file name
+    inventory_hostname = list(hosts.all.options['inventory_manager']._inventory.hosts.keys())[0]
+    managed_report = f"{TMP_DIRECTORY}/gather_diagnostics_report_managed_node_{inventory_hostname}.yml"
+    try:
+        setup_test_directory(hosts, TMP_DIRECTORY)
+        hosts.all.set_fact(
+            gather_diagnostics_output_dir=TMP_DIRECTORY
+        )
+        results = hosts.all.include_role(
+            name="gather_diagnostics"
+        )
+        assert_module_did_not_fail(results)
+        assert_controller_file_exists(hosts, control_report)
+        assert_controller_file_exists(hosts, managed_report)
+    finally:
+        cleanup_test_directory(hosts, TMP_DIRECTORY)
+
+def test_gather_diagnostics_role_with_syslog_false(ansible_zos_module):
+    hosts = ansible_zos_module
+    control_report = f"{TMP_DIRECTORY}/gather_diagnostics_report_control.yml"
+    # Get the inventory hostname from the hosts object to build the diagnostic file name
+    inventory_hostname = list(hosts.all.options['inventory_manager']._inventory.hosts.keys())[0]
+    managed_report = f"{TMP_DIRECTORY}/gather_diagnostics_report_managed_node_{inventory_hostname}.yml"
+    try:
+        setup_test_directory(hosts, TMP_DIRECTORY)
+        hosts.all.set_fact(
+            gather_diagnostics_output_dir=TMP_DIRECTORY,
+            gather_diagnostics_gather_syslog=False
+        )
+        results = hosts.all.include_role(
+            name="gather_diagnostics"
+        )
+        assert_module_did_not_fail(results)
+        assert_controller_file_exists(hosts, control_report)
+        assert_controller_file_exists(hosts, managed_report)
+
+    finally:
+        cleanup_test_directory(hosts, TMP_DIRECTORY)
+
+def test_gather_diagnostics_role_with_log_false(ansible_zos_module):
+    hosts = ansible_zos_module
+    control_report = f"{TMP_DIRECTORY}/gather_diagnostics_report_control.yml"
+    # Get the inventory hostname from the hosts object
+    inventory_hostname = list(hosts.all.options['inventory_manager']._inventory.hosts.keys())[0]
+    managed_report = f"{TMP_DIRECTORY}/gather_diagnostics_report_managed_node_{inventory_hostname}.yml"
+    try:
+        setup_test_directory(hosts, TMP_DIRECTORY)
+        hosts.all.set_fact(
+            gather_diagnostics_output_dir=TMP_DIRECTORY,
+            gather_diagnostics_save_control_log=False,
+            gather_diagnostics_save_managed_node_log=False
+        )
+        results = hosts.all.include_role(
+            name="gather_diagnostics"
+        )
+        assert_module_did_not_fail(results)
+        assert_controller_file_does_not_exist(hosts, control_report)
+        assert_controller_file_does_not_exist(hosts, managed_report)
+
+    finally:
+        cleanup_test_directory(hosts, TMP_DIRECTORY)

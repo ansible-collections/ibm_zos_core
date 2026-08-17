cd3ebcb67198f2f3411cc9ab7ff3027306a3ca7f
Yogesh Rana <Yogesh.Rana@ibm.com>
2026-08-17 22:53:03 +0530

    migrate from deprecated ansible.module_utils._text to common.text.converters
    Signed-off-by: Yogesh Rana <Yogesh.Rana@ibm.com>

--- plugins/modules/zos_operator.py
+++ plugins/modules/zos_operator.py
@@ -216,7 +216,7 @@ changed:
 import traceback
 from timeit import default_timer as timer
 from ansible.module_utils.basic import AnsibleModule
-from ansible.module_utils._text import to_text
+from ansible.module_utils.common.text.converters import to_text
 from ansible_collections.ibm.ibm_zos_core.plugins.module_utils.ansible_module import (
     AnsibleModuleHelper,
 )

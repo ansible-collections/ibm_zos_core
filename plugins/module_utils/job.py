9bcf5e93f06187a23a365a01aad6bcfb7a35f105
hmillersarah <hmillersarah@gmail.com>
2026-09-11 13:45:37 -0400

    update job not found outcomes from ZOAU v1.4.3.0 changes to remain compatible with zos_core v2.0.0
    Signed-off-by: hmillersarah <hmillersarah@gmail.com>

--- plugins/module_utils/job.py
+++ plugins/module_utils/job.py
@@ -103,9 +103,10 @@ def job_output(job_id=None, owner=None, job_name=None, dd_name=None, sysin=False
         "job_name": job_name,
         "dd_name": dd_name
     })
-    job_id = parsed_args.get("job_id") or "*"
+    job_id = parsed_args.get("job_id")
+    # Defaults to wildcard
     job_name = parsed_args.get("job_name") or "*"
-    owner = parsed_args.get("owner") or "*"
+    owner = parsed_args.get("owner")
     dd_name = parsed_args.get("dd_name") or ""
 
     job_detail = _get_job_status(
--- plugins/modules/zos_job_output.py
+++ plugins/modules/zos_job_output.py
@@ -46,16 +46,19 @@ options:
         (e.g "STC02560", "STC*")
     type: str
     required: false
+    default: null
   job_name:
     description:
       - The name of the batch job. (e.g "TCPIP", "C*")
     type: str
     required: false
+    default: "*"
   owner:
     description:
       - The owner who ran the job. (e.g "IBMUSER", "*")
     type: str
     required: false
+    default: null
   dd_name:
     description:
       - Data definition name (show only this DD on a found job).
@@ -510,9 +513,9 @@ def run_module():
         Any exception while fetching jobs.
     """
     module_args = dict(
-        job_id=dict(type="str", required=False),
-        job_name=dict(type="str", required=False),
-        owner=dict(type="str", required=False),
+        job_id=dict(type="str", required=False, default=None),
+        job_name=dict(type="str", required=False, default="*"),
+        owner=dict(type="str", required=False, default=None),
         dd_name=dict(type="str", required=False, aliases=['ddname']),
         sysin_dd=dict(type="bool", required=False, default=False),
     )
@@ -675,7 +678,6 @@ def run_module():
         module.fail_json(msg=repr(e), **results)
 
     module.exit_json(**results)
-
 
 def main():
     run_module()
